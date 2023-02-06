b12b4862274319439d178d2521b659e2763b7f51
hyeongseop kim <times21c@gmail.com>
2023-02-06 11:31:27 +0900

    Time: 64 ms (77.34%), Space: 15.2 MB (18.92%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dct = {}
+        for idx, num in enumerate(nums):
+            diff = target - num
+            if diff in dct: 
+                return [dct[diff], idx]
+            dct[num] = idx
