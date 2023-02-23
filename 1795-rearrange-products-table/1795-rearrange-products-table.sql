47f40403078787b17b72e697fd5304638106030b
hyeongseop kim <times21c@gmail.com>
2023-02-23 15:04:54 +0900

    Time: 1666 ms (5.02%), Space: 0B (100.00%) - LeetHub

--- 1795-rearrange-products-table/1795-rearrange-products-table.sql
+++ 1795-rearrange-products-table/1795-rearrange-products-table.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 SELECT product_id, 'store1' AS store, store1 AS price FROM Products WHERE NOT store1 IS NULL
-UNION ALL 
+UNION 
 SELECT product_id, 'store2', store2 AS price FROM Products WHERE NOT store2 IS NULL
-UNION ALL
+UNION
 SELECT product_id, 'store3', store3 AS price FROM Products WHERE NOT store3 IS NULL
