b925b18fc010281a58c898c4ca889a3bb3fe7b32
hyeongseop kim <times21c@gmail.com>
2023-02-25 18:41:01 +0900

    Time: 35 ms (48.38%), Space: 13.8 MB (47.89%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
@@ -1,14 +1,14 @@
 class Solution:
     def isStrictlyPalindromic(self, n: int) -> bool:
-        def convert(n, base):
+        def convert_n(n, base):
             if n < 1:
                 return ''
             n, mod = divmod(n, base)
-            return convert(n, base) + str(mod)
+            return convert_n(n, base) + str(mod)
         for base in range(2, n - 1):
-            converted_n = convert(n, base)
-            if converted_n != converted_n[::-1]:
-                return False  
+            converted_n = convert_n(n, base)
+            if converted_n != converted_n[::-1]: 
+                return False
         return True
 
         
