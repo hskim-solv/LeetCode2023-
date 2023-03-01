d47c290fc23b40365ef1924a35a396e689a3849a
hyeongseop kim <times21c@gmail.com>
2023-03-01 22:55:34 +0900

    Time: 73 ms (75.63%), Space: 14.1 MB (56.90%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.py
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.py
@@ -1,16 +1,19 @@
 class Solution:
     def leftRigthDifference(self, nums: List[int]) -> List[int]:
+        '''
+        # 1
         total, cum = sum(nums), 0
         answar = []
         for n in nums:
             answar.append(abs(total - n - cum * 2))
             cum += n
         return answar
-        '''
+        
         # 2
         cum = list(itertools.accumulate(nums))
         total = cum[-1]
         return [abs(total- c*2 + n) for n,c in zip(nums,cum)]
+        '''
         # 3
         left = 0
         right = sum(nums)
@@ -20,5 +23,5 @@ class Solution:
             ans.append(abs(left-right))
             right -= n
         return ans
-        '''
+        
         
