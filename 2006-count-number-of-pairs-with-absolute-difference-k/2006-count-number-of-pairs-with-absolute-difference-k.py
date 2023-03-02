fe9ede21926b8517e32e1568e21e4d5a11d4636e
hyeongseop kim <times21c@gmail.com>
2023-03-02 21:24:18 +0900

    Time: 92 ms (70.60%), Space: 13.8 MB (58.24%) - LeetHub

--- 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.py
+++ 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.py
@@ -4,7 +4,6 @@ class Solution:
         nums.reverse()
         while nums:
             n = nums.pop()
-            print(nums,abs(n+k),nums.count(abs(n-k)),ans)
             ans += nums.count(n+k)
             ans += nums.count(n-k)
         return ans
