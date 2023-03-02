5250bfae06ff56880b8edbf2dba67dd7075b9b2d
hyeongseop kim <times21c@gmail.com>
2023-03-03 00:55:47 +0900

    Time: 34 ms (55.40%), Space: 13.8 MB (52.30%) - LeetHub

--- 2103-relin-hcrings-and-rods-relin-hc/2103-relin-hcrings-and-rods-relin-hc.py
+++ 2103-relin-hcrings-and-rods-relin-hc/2103-relin-hcrings-and-rods-relin-hc.py
@@ -2,6 +2,6 @@ class Solution:
     def countPoints(self, rings: str) -> int:
         rgb = { i:[] for i in set(rings[1::2]) }
         for i in range(0, len(rings), 2):
-            rgb[rings[i+1]].append(rings[i])
+            rgb[rings[i+1]] += rings[i]
         return sum( len(set(x))==3 for x in rgb.values())
         
