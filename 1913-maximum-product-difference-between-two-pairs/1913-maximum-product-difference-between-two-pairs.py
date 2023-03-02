131cc88262f5a1aa4f93d2b7a39611d05eeef7cc
hyeongseop kim <times21c@gmail.com>
2023-03-03 01:09:33 +0900

    Time: 174 ms (69.02%), Space: 15.4 MB (59.98%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.py
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.py
@@ -1,6 +1,3 @@
 class Solution:
     def maxProductDifference(self, nums: List[int]) -> int:
-        
-        #return reduce(lambda x,y: x*y,heapq.nlargest(2, nums)) - reduce(lambda x,y: x*y,heapq.nsmallest(2, nums))
-        nums.sort()
-        return nums[-2]*nums[-1] - nums[0]*nums[1]
+        return prod(heapq.nlargest(2, nums)) - prod(heapq.nsmallest(2, nums))
