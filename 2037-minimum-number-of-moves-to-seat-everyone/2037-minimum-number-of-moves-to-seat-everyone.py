b010bc2d1a89d399dc7453740eeba9ebaec9152f
hyeongseop kim <times21c@gmail.com>
2023-03-02 23:56:04 +0900

    Time: 66 ms (62.87%), Space: 13.8 MB (51.98%) - LeetHub

--- 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.py
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.py
@@ -1,7 +1,4 @@
 class Solution:
     def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
-        #return sum(abs(seat-student) for seat, student in zip(sorted(seats), sorted(students)))
+        return sum(abs(seat-student) for seat, student in zip(sorted(seats), sorted(students)))
         
-        seats.sort()
-        students.sort()
-        return sum(abs(e - t) for e, t in zip(seats, students))
