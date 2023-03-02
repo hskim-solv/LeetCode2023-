a1b261de5af413d5e1474c91f63929a3ffdc4760
hyeongseop kim <times21c@gmail.com>
2023-03-03 02:02:33 +0900

    Time: 111 ms (94.39%), Space: 14.4 MB (79.40%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,5 +1,4 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-
-        return [ names[i] for i, _ in sorted(enumerate(heights), key = lambda x:x[1]) ][::-1]
+        return [names[i] for i, n in sorted(enumerate(heights), key =lambda x:x[1]) ][::-1]
         
