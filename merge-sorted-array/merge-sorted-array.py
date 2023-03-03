84928d7078dfda687d7b56b654581a931a9e9eec
hyeongseop kim <times21c@gmail.com>
2023-03-03 13:51:44 +0900

    Time: 37 ms, Memory: 14.1 MB - LeetHub

--- merge-sorted-array/merge-sorted-array.py
+++ merge-sorted-array/merge-sorted-array.py
@@ -3,13 +3,13 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        start = 0
+        i = 0
         m -= 1
         for n2 in nums2:
-            for i in range(start, m + n + 1):
+            for i in range(i, m + n + 1):
                 if nums1[i] > n2 or i > m:
                     nums1.insert(i, n2)
                     del nums1[-1]
                     m += 1
-                    start = i + 1
+                    i += 1
                     break
