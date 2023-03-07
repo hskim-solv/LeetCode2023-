dd48731767978bdd45987ec533567f27d3d31b8e
hyeongseop kim <times21c@gmail.com>
2023-03-07 18:02:05 +0900

    Time: 64 ms (92.10%), Space: 14.2 MB (43.53%) - LeetHub

--- 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.py
+++ 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.py
@@ -1,6 +1,5 @@
 class Solution:
     def kthDistinct(self, arr: List[str], k: int) -> str:
-        o = [k for k,v in Counter(arr).items() if v == 1]
-        if len(o) >= k:
-            return o[k-1]
-        return ""
+        o = list(k for k, v in Counter(arr).items() if v == 1)
+        return o[k-1] if len(o) > k-1 else ""
+
