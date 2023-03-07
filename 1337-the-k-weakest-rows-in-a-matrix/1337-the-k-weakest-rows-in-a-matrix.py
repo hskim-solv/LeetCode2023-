0709d21e91fdacb217f56994209a66451f6766e2
hyeongseop kim <times21c@gmail.com>
2023-03-07 13:49:50 +0900

    Time: 108 ms (82.70%), Space: 14.2 MB (81.02%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
@@ -1,9 +1,11 @@
 class Solution:
     def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
-        return (y for _, y in sorted([ 
-                (sum( takewhile(lambda x: x, row) ) , i)                          for i, row in enumerate(mat) 
-                    ])[:k] 
-        ) 
+        return [
+            y for _, y in sorted(
+                [ 
+                [sum( takewhile(lambda x: x, row) ) , i]                                        for i, row in enumerate(mat) 
+                ])[:k] 
+         ] 
 
         
              
