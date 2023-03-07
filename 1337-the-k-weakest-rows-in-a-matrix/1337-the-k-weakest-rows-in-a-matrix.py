7900fe375988e5c32d37d8723e1ebf9ea716e1d9
hyeongseop kim <times21c@gmail.com>
2023-03-07 13:34:18 +0900

    Time: 100 ms (98.43%), Space: 14.1 MB (81.02%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
@@ -1,5 +1,12 @@
 class Solution:
     def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        return [y for x,y in sorted( 
+                    [ 
+                        (sum( takewhile(lambda x: x, row) ) , i)                          for i, row in enumerate(mat) 
+                    ] 
+                )[:k]]
+    
+        '''
         return list( 
                 list(
                     zip(
@@ -12,5 +19,6 @@ class Solution:
             
             )[1]
     )[:k] 
+    '''
         
              
