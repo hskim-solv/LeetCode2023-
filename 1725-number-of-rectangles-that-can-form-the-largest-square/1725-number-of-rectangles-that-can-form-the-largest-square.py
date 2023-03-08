98cbe3cde08b6d9a4483ad54c5523e2e9978b282
hyeongseop kim <times21c@gmail.com>
2023-03-09 01:19:58 +0900

    Time: 175 ms (99.25%), Space: 14.5 MB (16.60%) - LeetHub

--- 1725-number-of-rectangles-that-can-form-the-largest-square/1725-number-of-rectangles-that-can-form-the-largest-square.py
+++ 1725-number-of-rectangles-that-can-form-the-largest-square/1725-number-of-rectangles-that-can-form-the-largest-square.py
@@ -1,4 +1,4 @@
 class Solution:
     def countGoodRectangles(self, rectangles: List[List[int]]) -> int:
         
-        return (rectangles := list(map(min,rectangles))).count(max(rectangles))
+        return (square := list(map(min,rectangles))).count(max(square))
