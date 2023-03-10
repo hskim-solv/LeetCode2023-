5ae9d5050bc4d7efdadeea9355c4b26b95e6de20
hyeongseop kim <times21c@gmail.com>
2023-03-11 02:10:25 +0900

    Time: 47 ms (97.59%), Space: 14.6 MB (93.16%) - LeetHub

--- 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.py
+++ 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.py
@@ -6,11 +6,11 @@
 #         self.right = right
 class Solution:
     def evaluateTree(self, root: Optional[TreeNode]) -> bool:
-        if root.val == 0:
-            return False
-        if root.val == 1:
-            return True
         if root.val == 2:
             return self.evaluateTree(root.left) or self.evaluateTree(root.right)
         if root.val == 3:
             return self.evaluateTree(root.left) and self.evaluateTree(root.right)
+        if root.val:
+            return True
+        return False
+
