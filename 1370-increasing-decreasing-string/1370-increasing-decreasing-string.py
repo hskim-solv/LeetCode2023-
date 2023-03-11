448b606b819a1f14de684a8bce59616f4e637c45
hyeongseop kim <times21c@gmail.com>
2023-03-11 10:22:49 +0900

    Time: 600 ms (5.11%), Space: 13.8 MB (74.64%) - LeetHub

--- 1370-increasing-decreasing-string/1370-increasing-decreasing-string.py
+++ 1370-increasing-decreasing-string/1370-increasing-decreasing-string.py
@@ -3,19 +3,15 @@ class Solution:
         res = ""
         s = sorted(s)
         ss = sorted((set(s)))
-        while s:
+        while any(s):
             for ch in ss:
-                if ch in s:
+                if ch and ch in s:
                     res += ch
-                    del s[s.index(ch)]
+                    s[s.index(ch)] = None
                     if ch not in s:
-                        ss[ss.index(ch)] =None
+                        ss[ss.index(ch)] = None
                         
-
             s = s[::-1]
             ss = ss[::-1]
-
-                    
-            
 
         return res     
