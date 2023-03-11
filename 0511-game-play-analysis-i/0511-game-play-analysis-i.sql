9cf45e8ae401c7abf6def4baab4bd26bfd0ff66f
hyeongseop kim <times21c@gmail.com>
2023-03-11 15:12:10 +0900

    Time: 962 ms (47.08%), Space: 0B (100.00%) - LeetHub

--- 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
@@ -1,4 +1,2 @@
 # Write your MySQL query statement below
 SELECT player_id, MIN(event_date) AS first_login FROM Activity GROUP BY player_id
-#SELECT player_id, event_date AS first_login FROM Activity 
-#WHERE event_date = (SELECT played_id, MIN(event_date) FROM Activity GROUP BY player_id)
