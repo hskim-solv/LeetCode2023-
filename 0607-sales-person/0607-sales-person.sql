72e08f0b25f1964243cc8b1bdddbcd0b308a329f
hyeongseop kim <times21c@gmail.com>
2023-03-11 17:00:55 +0900

    Time: 2744 ms (32.26%), Space: 0B (100.00%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
 
-SELECT name FROM SalesPerson s WHERE sales_id NOT IN (SELECT DISTINCT sales_id FROM Orders JOIN (SELECT com_id, name FROM Company WHERE name = "RED") c USING(com_id) )
+SELECT name FROM SalesPerson WHERE sales_id NOT IN (SELECT DISTINCT sales_id FROM Orders JOIN Company USING(com_id) WHERE name = "RED")
