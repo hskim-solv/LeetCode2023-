3bfaa300924d5026f4dd9c52c126a08ac5042795
hyeongseop kim <times21c@gmail.com>
2023-03-11 10:29:28 +0900

    Time: 90 ms (32.11%), Space: 14.1 MB (11.86%) - LeetHub

--- 1370-increasing-decreasing-string/1370-increasing-decreasing-string.py
+++ 1370-increasing-decreasing-string/1370-increasing-decreasing-string.py
@@ -1,17 +1,13 @@
+
 class Solution:
     def sortString(self, s: str) -> str:
-        res = ""
-        s = sorted(s)
-        ss = sorted((set(s)))
-        while any(s):
-            for ch in ss:
-                if ch and ch in s:
-                    res += ch
-                    s[s.index(ch)] = None
-                    if ch not in s:
-                        ss[ss.index(ch)] = None
-                        
-            s = s[::-1]
-            ss = ss[::-1]
-
-        return res     
+        s = list(s)
+        result = ''
+        while s:
+            for letter in sorted(set(s)):
+                s.remove(letter)
+                result += letter
+            for letter in sorted(set(s), reverse=True):
+                s.remove(letter)
+                result += letter
+        return result
