87d13ab99c349beab11015d3d7c60da2c63ff03e
hyeongseop kim <times21c@gmail.com>
2023-03-13 02:28:11 +0900

    Time: 682 ms (5.09%), Space: 13.9 MB (31.47%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -3,11 +3,10 @@ class Solution:
         
         #even_numbers = (n for n in numbers if not n % 2)
         #print(list(takewhile(lambda x: x != i, (j for j in t for i in s if i==j ))))
-        x,l = 0 , len(s)
-        t, s = deque(t), deque(s)
-        while s:
-            i = s.popleft() 
-            while t:
+        x, l = 0 , len(s)
+
+        for i in s:
+            while t := deque(t):
                 if i == t.popleft():
                     x+=1
                     break 
