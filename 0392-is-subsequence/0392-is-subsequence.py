019ac0f4b390e7cfbc8ae9c7bcdd7dee50dcb7e3
hyeongseop kim <times21c@gmail.com>
2023-03-13 02:36:59 +0900

    Time: 34 ms (62.20%), Space: 13.9 MB (71.08%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -2,9 +2,9 @@ class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         l = len(s)
         for i in s:
-            while t := deque(t):
-                if i == t.popleft():
-                    l -= 1
-                    break 
+                if i in t:
+                    l -= 1 
+                    t = t[t.index(i)+1:]
+                    print(l)
         return not l
        
