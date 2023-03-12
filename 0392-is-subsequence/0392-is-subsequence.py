332e3a0d2ac3c8d63026da4dc0423de62b13f53c
hyeongseop kim <times21c@gmail.com>
2023-03-13 02:31:33 +0900

    Time: 678 ms (5.09%), Space: 14 MB (31.47%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,10 +1,9 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        
-        #even_numbers = (n for n in numbers if not n % 2)
-        #print(list(takewhile(lambda x: x != i, (j for j in t for i in s if i==j ))))
-        l = len(s)
+        if len(s) > len(t):return False
+        if len(s) == 0:return True
 
+        l = len(s)
         for i in s:
             while t := deque(t):
                 if i == t.popleft():
