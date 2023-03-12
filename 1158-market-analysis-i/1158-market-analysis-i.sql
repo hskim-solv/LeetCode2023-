81522c38450c13822cae61a94bf2d9d110b15312
hyeongseop kim <times21c@gmail.com>
2023-03-12 17:41:19 +0900

    Time: 2073 ms (83.66%), Space: 0B (100.00%) - LeetHub

--- 1158-market-analysis-i/1158-market-analysis-i.sql
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -2,4 +2,4 @@
 SELECT u.user_id AS buyer_id, u.join_date, 
         COUNT(o.order_id) AS orders_in_2019 
 FROM Users u LEFT JOIN Orders o  
-ON u.user_id = o.buyer_id AND YEAR(order_date) = 2019 GROUP BY 1
+ON YEAR(order_date) = 2019 AND u.user_id = o.buyer_id  GROUP BY 1
