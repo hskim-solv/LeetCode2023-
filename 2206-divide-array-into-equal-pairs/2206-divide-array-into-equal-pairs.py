c2d3529dfddef8f71aaaf6a89d49b3be28b8a560
hyeongseop kim <times21c@gmail.com>
2023-03-16 00:36:15 +0900

    Time: 87 ms (83.92%), Space: 14.1 MB (50.97%) - LeetHub

--- 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.py
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.py
@@ -1,4 +1,4 @@
 class Solution:
     def divideArray(self, nums: List[int]) -> bool:
-        return not any(x & 1 for x in Counter(nums).values())  
+        return all(x %2 == 0 for x in Counter(nums).values())  
       
