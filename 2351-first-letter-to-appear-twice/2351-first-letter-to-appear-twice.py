d60327a0fcfbde883361eb23320d0755ec4c7b4e
hyeongseop kim <times21c@gmail.com>
2023-03-15 22:15:51 +0900

    Time: 39 ms (15.30%), Space: 13.9 MB (39.47%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.py
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.py
@@ -1,8 +1,7 @@
 class Solution:
     def repeatedCharacter(self, s: str) -> str:
-        for i in range(len(s)):
-            l = []
-            for letter in s:
-                if letter in l:
-                    return letter
-                l.append(letter)
+        l = []
+        for letter in s:
+            if letter in l:
+                return letter
+            l.append(letter)
