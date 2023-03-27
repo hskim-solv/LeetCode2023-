3bbf5bc656c0b8081a7157027aaa0a609c33bbf9
hyeongseop kim <times21c@gmail.com>
2023-03-27 20:54:00 +0900

    Time: 70 ms (95.39%), Space: 14 MB (47.20%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.py
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.py
@@ -1,15 +1,12 @@
 class Solution:
     def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
         d = {}
-        ans = []
         for i, s in enumerate(groupSizes):
             if s in d:
                 d[s].append(i)
             else:
                 d[s] = [i]
-        for k, v in d.items():
-            for _ in range(len(d[k])//k):
-                ans.append(d[k][:k])
-                del d[k][:k]
-        return ans
+        return [  v[k*i:k*(i+1)] for k, v in d.items() for i in range(len(v) // k) ]
+
+        
                 
