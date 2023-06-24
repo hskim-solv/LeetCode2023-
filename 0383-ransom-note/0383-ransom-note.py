d106b299e0013a59e958c5e8dee13e0ad8f11df0
hyeongseop kim <times21c@gmail.com>
2023-06-25 08:56:58 +0900

    Time: 65 ms (77.43%), Space: 16.7 MB (27.59%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,11 +1,3 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        ransomNote, magazine = list(Counter(ransomNote).items()), Counter(magazine)
-        while ransomNote:
-            n = ransomNote.pop()
-            if n[0] not in magazine:
-                return False
-            else:
-                if n[1] > magazine[n[0]]:
-                    return False
-        return True
+        return Counter(ransomNote) <= Counter(magazine)
