b88dad4b0e5e080a068edecf369af0ca67a4dd3e
hyeongseop kim <times21c@gmail.com>
2023-07-03 15:55:16 +0900

    Time: 230 ms (86.43%), Space: 23.5 MB (70.02%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -0,0 +1,15 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = []
+        total = reduce(lambda x, y: x * y, nums, 1)
+        for i in range(len(nums)):
+            if not total:
+                if not nums[i]:
+                    nnl = nums[:i] + nums[i+1:]
+                    if 0 not in nums[:i]:
+                        res.append(reduce(lambda x, y: x * y, nnl, 1))
+                        continue
+                res.append(total)
+            else:
+                res.append(total//nums[i])
+        return res
