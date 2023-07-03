c85f67dac549d685b58ca45113b3cddda498fb8d
hyeongseop kim <times21c@gmail.com>
2023-07-03 16:20:05 +0900

    Time: 234 ms (77.28%), Space: 23.7 MB (54.95%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -3,8 +3,10 @@ class Solution:
         if 0 not in nums:
             total = reduce(lambda x, y: x * y, nums, 1)
             return [total//nums[i] for i in range(len(nums))]
-        elif nums.count(0) == 1:
-            i = nums.index(0)
-            return [0] * i + [reduce(lambda x, y: x * y, nums[:i]+nums[i+1:], 1)] + [0] *(len(nums)-i-1)
-        else:
+        elif nums.count(0) > 1:
             return [0] * len(nums)
+        else:
+            i = nums.index(0)
+            res = [0] * len(nums)
+            res[i] = reduce(lambda x, y: x * y, nums[:i], 1) * reduce(lambda x, y: x * y, nums[i+1:], 1)
+            return res
