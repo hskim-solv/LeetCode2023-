0a00ba9287ee9037576219778ed197efe63bf2ef
hyeongseop kim <times21c@gmail.com>
2023-07-03 15:56:28 +0900

    Time: 227 ms (91.39%), Space: 23.7 MB (54.95%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -6,7 +6,7 @@ class Solution:
             if not total:
                 if not nums[i]:
                     nnl = nums[:i] + nums[i+1:]
-                    if 0 not in nums[:i]:
+                    if 0 not in nnl:
                         res.append(reduce(lambda x, y: x * y, nnl, 1))
                         continue
                 res.append(total)
