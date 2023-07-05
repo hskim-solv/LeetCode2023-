9458162f13d45ade4485c7fe2d698318ff8764eb
hyeongseop kim <times21c@gmail.com>
2023-07-05 19:34:30 +0900

    Time: 53 ms (22.70%), Space: 16.3 MB (80.59%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,11 @@
+class Solution:
+    def __init__(self):
+        self.d = {(1,1):1,(2,1):1}
+    def uniquePaths(self, m: int, n: int) -> int:
+        if n > m:
+            m, n = n, m
+        if n == 1:
+            return 1
+        if (m, n) not in self.d:
+            self.d[(m, n)] = self.uniquePaths(m-1, n) + self.uniquePaths(m, n-1)
+        return self.d[(m, n)]
