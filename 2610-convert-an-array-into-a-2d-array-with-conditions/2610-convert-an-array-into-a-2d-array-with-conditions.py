1d681f1d091e2a1c7d17b995dc6ddc676ec0f2b4
hyeongseop kim <times21c@gmail.com>
2023-07-08 02:05:00 +0900

    Time: 71 ms (56.73%), Space: 16.2 MB (99.70%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.py
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.py
@@ -1,14 +1,14 @@
 class Solution:
     def findMatrix(self, nums: List[int]) -> List[List[int]]:
         res = []
+        length = 0
         for n, freq in Counter(nums).most_common():
-            length = len(res)
             if length < freq:
                 for i in range(length):
                     res[i].append(n)
                 for _ in range(freq-length):
                     res.append([n])
-
+                length = freq
             else:
                 for i in range(freq):
                     res[i].append(n)
