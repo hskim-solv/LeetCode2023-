ffef609a5462c4da1772e8393d4ca589634a9d4e
hyeongseop kim <times21c@gmail.com>
2023-07-12 14:26:41 +0900

    Time: 819 ms (7.31%), Space: 32.6 MB (18.04%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,8 +1,5 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        if len(nums)==2:
-            if nums[0]==nums[1]:
-                return nums[0]
         nums.sort()
         l = len(nums)//2
         print(nums)
