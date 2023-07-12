ff84346ffd8ed6688bcfc303e6c50bc5f5583f58
hyeongseop kim <times21c@gmail.com>
2023-07-12 13:56:38 +0900

    Time: 663 ms (69.38%), Space: 16.8 MB (6.43%) - LeetHub

--- 0454-4sum-ii/0454-4sum-ii.py
+++ 0454-4sum-ii/0454-4sum-ii.py
@@ -8,14 +8,13 @@ class Solution:
                     d2[k2+k1] = 0
                 d2[k2+k1] += v1*v2
         d3={}
-        for k2,v2 in d2.items():
+        for k2,v2 in Counter(nums4).items():
             for k3,v3 in Counter(nums3).items():
                 if k3+k2 not in d3:
                     d3[k2+k3] = 0
                 d3[k2+k3] += v3*v2
-        c4 = Counter(nums4)
         for k3,v3 in d3.items():
-                if -k3 in c4:
-                    cnt += c4[-k3]*v3
+                if -k3 in d2:
+                    cnt += d2[-k3]*v3
 
         return cnt
