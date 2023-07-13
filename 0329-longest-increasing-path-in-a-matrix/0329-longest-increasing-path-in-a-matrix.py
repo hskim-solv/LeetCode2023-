85db8373de04e0fa9353da8f88824ec2cd2e3bfc
hyeongseop kim <times21c@gmail.com>
2023-07-13 14:54:21 +0900

    Time: 416 ms (78.98%), Space: 18.6 MB (65.12%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -8,27 +8,27 @@ class Solution:
         for m in range(self.m):
             for n in range(self.n):
                 if (m,n) not in self.d:
-                    self.d[(m,n)] = self.search(matrix,m,n,1)
+                    self.d[(m,n)] = self.search(m,n,1)
                 res = max(res,self.d[(m,n)])
         return res
         
-    def search(self,matrix,i,j,res):
-        point = matrix[i][j]    
+    def search(self,i,j,res):
+        point = self.matrix[i][j]    
         new_res =res
-        if self.m-1 > i and matrix[i+1][j] > point: #up
+        if self.m-1 > i and self.matrix[i+1][j] > point: #up
             if (i+1,j) not in self.d:
-                self.d[(i+1,j)] = self.search(matrix,i+1,j,1)
+                self.d[(i+1,j)] = self.search(i+1,j,1)
             new_res = max(new_res,(self.d[(i+1,j)]+res))
-        if 0 < i and matrix[i-1][j] > point: #down
+        if 0 < i and self.matrix[i-1][j] > point: #down
             if (i-1,j) not in self.d:
-                self.d[(i-1,j)] = self.search(matrix,i-1,j,1)
+                self.d[(i-1,j)] = self.search(i-1,j,1)
             new_res = max(new_res,(self.d[(i-1,j)]+res))
-        if 0 < j and matrix[i][j-1] > point: #left
+        if 0 < j and self.matrix[i][j-1] > point: #left
             if (i,j-1) not in self.d:
-                self.d[(i,j-1)] = self.search(matrix,i,j-1,1)
+                self.d[(i,j-1)] = self.search(i,j-1,1)
             new_res = max(new_res,(self.d[(i,j-1)]+res))
-        if self.n-1 > j and matrix[i][j+1] > point: #right
+        if self.n-1 > j and self.matrix[i][j+1] > point: #right
             if (i,j+1) not in self.d:
-                self.d[(i,j+1)] = self.search(matrix,i,j+1,1)
+                self.d[(i,j+1)] = self.search(i,j+1,1)
             new_res = max(new_res,(self.d[(i,j+1)]+res))
         return new_res
