69e2ce898e0d546dded0374236dad4d807016f3a
hyeongseop kim <times21c@gmail.com>
2023-07-13 15:58:16 +0900

    Time: 34 ms (98.85%), Space: 16.3 MB (76.19%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,12 +1,12 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         self.d = {}
-        def inner(nums,idx):
-            if not nums:
+        def inner(idx):
+            if len(nums)-idx == 0:
                 return 0
-            if len(nums) < 3:
-                return max(nums)
+            if len(nums)-idx < 3:
+                return max(nums[idx:])
             if idx not in self.d:
-                self.d[idx] = nums[0]+max(inner(nums[2:],idx+2),inner(nums[3:],idx+3))
+                self.d[idx] = nums[idx]+max(inner(idx+2),inner(idx+3))
             return self.d[idx]
-        return max(inner(nums,0),inner(nums[1:],1))
+        return max(inner(0),inner(1))
