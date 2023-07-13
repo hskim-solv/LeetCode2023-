99a963c2a0ccc164e50683ad0705a6b1182e7b33
hyeongseop kim <times21c@gmail.com>
2023-07-13 11:28:37 +0900

    Time: 104 ms (5.71%), Space: 16.4 MB (40.22%) - LeetHub

--- 2609-find-the-longest-balanced-substring-of-a-binary-string/2609-find-the-longest-balanced-substring-of-a-binary-string.py
+++ 2609-find-the-longest-balanced-substring-of-a-binary-string/2609-find-the-longest-balanced-substring-of-a-binary-string.py
@@ -1,23 +1,32 @@
 class Solution:
     def __init__(self):
-        self.d=[]
+        self.d = []
     def findTheLongestBalancedSubstring(self, s: str) -> int:
-        if s in self.d:
+        if len(set(s))==1:
             return 0
         length = len(s)
-        if length == 1:
-            return 0
-        l, r = set(s[:length//2]), set(s[length//2:])
-
-        if len(l) ==1 and len(r)==1:
-            if '1' not in l and '0' not in r:
-                if length % 2 == 0:
-                    return length
-        self.d.append(s)
-        return max(
-            
-            self.findTheLongestBalancedSubstring(s[1:]),
-                   self.findTheLongestBalancedSubstring(s[:-1])
-            
-        )
+        cnt = 0
+        l = []
+        tmp = []
+        for k,g in groupby(s):
+            lg = list(g)
+            cnt += len(lg)
+            tmp += lg
+            if k == '1':
+                l.append("".join(tmp))
+                tmp = []
+            if cnt >= length:
+                break
+        if len(l) > 1:
+            return max(map(self.findTheLongestBalancedSubstring,l))
+        else:
+            if s[0]=='0'and s[-1]=='1' and s.count('1') == s.count('0'):
+                return length
+            if s in self.d:
+                return 0
+            self.d.append(s)
+            return max(
+                        self.findTheLongestBalancedSubstring(s[1:]),
+                       self.findTheLongestBalancedSubstring(s[:-1]) 
+            )
         
