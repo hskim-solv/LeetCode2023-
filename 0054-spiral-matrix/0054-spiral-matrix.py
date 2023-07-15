cb22b259e68c4db21c56a140742b965a68dd6ab7
hyeongseop kim <times21c@gmail.com>
2023-07-15 13:25:51 +0900

    Time: 39 ms (93.19%), Space: 16.4 MB (51.73%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -10,23 +10,14 @@ class Solution:
             div,mod = divmod(i,4)
             if row:
                 if mod == 0:
-                    #print('000',row[0],mod,div,res)
                     res += row.popleft()[div:-1-div]
-                    #print(res)
                 elif mod == 2:
-                 
-                    #print('222',row[-1],mod,div,res)
                     res += reversed(row.pop()[div:-1-div])
-                    #print(res)
             if col:
                 if mod == 1:
                     res += col.pop()[div:-div]
-
                 elif mod == 3:
-                    #print('333',col[0],mod,div,res)
                     res += reversed(col.popleft()[1+div:-1-div])
-                    #print(res)
-
             i += 1
 
         return res
