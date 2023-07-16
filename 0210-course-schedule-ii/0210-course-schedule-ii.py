3f60f73cb5b9a873de8731d84bb0e7e209cee47e
hyeongseop kim <times21c@gmail.com>
2023-07-16 10:03:03 +0900

    Time: 114 ms (72.46%), Space: 19.4 MB (46.54%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,7 +1,7 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         # use DFS to parse the course structure
-        self.graph = collections.defaultdict(list) # a graph for all courses
+        self.graph = defaultdict(list) # a graph for all courses
         self.res = []
         for pair in prerequisites:
             self.graph[pair[0]].append(pair[1]) 
@@ -13,14 +13,11 @@ class Solution:
         return self.res
     
     def DFS(self, node):
-        if self.visited[node] == -1: # cycle detected
-            return False
-        if self.visited[node] == 1:
-            return True # has been finished, and been added to self.res
+        if self.visited[node] == -1: return False # cycle detected
+        if self.visited[node] == 1: return True # has been finished, and been added to self.res
         self.visited[node] = -1 # mark as visited
         for x in self.graph[node]:
-            if not self.DFS(x):
-                return False
+            if not self.DFS(x): return False
         self.visited[node] = 1 # mark as finished
         self.res.append(node) # add to solution as the course depenedent on previous ones
         return True
