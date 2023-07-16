6167bf72962c2dfb753d95a3c3f6d775e8a7dc90
hyeongseop kim <times21c@gmail.com>
2023-07-17 08:04:43 +0900

    Time: 153 ms (89.23%), Space: 21.2 MB (53.87%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -2,7 +2,6 @@ class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         i = 1
         intervals.sort(key=lambda x:x[0])
-        #print(intervals)
         while i < len(intervals):
             if intervals[i-1][1] >= intervals[i][0]:
                 if intervals[i-1][1] < intervals[i][1]:
