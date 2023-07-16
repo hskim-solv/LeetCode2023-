8a36cb3c4fc2d75b94f108072b60df54a4ad5e56
hyeongseop kim <times21c@gmail.com>
2023-07-16 16:51:24 +0900

    Time: 614 ms (68.19%), Space: 38.7 MB (59.31%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -5,7 +5,7 @@
 #         self.next = next
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head or not head.next:
+        if not (head and head.next):
             return head
     
         slow, fast = head, head
