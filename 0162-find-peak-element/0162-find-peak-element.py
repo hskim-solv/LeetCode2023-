1e24e75ecd5edcbbabf9ce23ce6b9c3a441d4418
hyeongseop kim <times21c@gmail.com>
2023-07-19 23:05:10 +0900

    Time: 64 ms (41.59%), Space: 16.3 MB (99.52%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -2,7 +2,7 @@ class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         middle = len(nums)//2
         def find(nums):
-            if len(nums)<=3:
+            if len(nums)<3:
                 return max(nums)
             middle = len(nums)//2
             return max(find(nums[:middle]),find(nums[middle:]))
