f50f3905ac7d5c7e009f50a74ff69a9d6be01870
hyeongseop kim <times21c@gmail.com>
2023-07-24 07:22:18 +0900

    Time: 3797 ms (5.01%), Space: 36.4 MB (5.63%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -2,13 +2,14 @@ class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
         if len(set(nums)) < 3:
             return False
-
         i = nums.index(min(nums))
-        #print(i,nums)
+        ii = i+1
+        while ii < len(nums) and nums[i] == nums[ii]:
+            ii += 1
+        del nums[i+1:ii]
+        #print(nums)
+        i = nums.index(min(nums))
         if len(set(nums[i:])) < 3:
-            while i+1 < len(nums) and nums[i] == nums[i+1]:
-                #print(nums)
-                del nums[i+1]
             del nums[i]
             return self.increasingTriplet(nums)
     
@@ -16,7 +17,6 @@ class Solution:
             if nums[i] < max(nums[i+1:]):
                 k = nums.index(max(nums[i+1:]))
                 if len(set(nums[i:k+1])) < 3:
-                    #print(nums,'k')
                     return any([self.increasingTriplet(nums[i:k]+nums[k+1:]),self.increasingTriplet(nums[:k+1])])
                 elif nums[i] < max(nums[i+1:k]):
                     return True
