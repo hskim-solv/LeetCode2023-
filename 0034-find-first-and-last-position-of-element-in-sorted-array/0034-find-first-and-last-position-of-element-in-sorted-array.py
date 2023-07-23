2a28edd776453442f8ccb5d064776118a80bd62a
hyeongseop kim <times21c@gmail.com>
2023-07-23 19:47:44 +0900

    Time: 95 ms (77.26%), Space: 17.7 MB (72.83%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,16 +1,21 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        def helper(nums,target,base):
-            length = len(nums)
-            if length == 0:
-                return [-1,-1]
-            mid = len(nums) // 2
-            if target < nums[mid]:
-                return helper(nums[:mid],target,base)
-            elif target > nums[mid]:
-                return helper(nums[mid+1:],target,base+mid+1)
-            else:
-                idx = nums.index(nums[mid])
-                cnt = nums.count(nums[mid])-1
-                return [base+idx,base+idx+cnt]
-        return helper(nums,target,0)
+        return self.helper(nums,target,0)
+    
+    def helper(self, nums: List[int], target: int, base: int) -> List[int]:
+        if not nums:
+            return [-1,-1]
+        mid = len(nums) // 2
+        if not mid:
+            if nums[0]==target:
+                return [base,base]
+            return [-1,-1]
+
+        if target < nums[mid]:
+            return self.helper(nums[:mid],target,base)
+        elif target > nums[mid]:
+            return self.helper(nums[mid+1:],target,base+mid+1)
+        else:
+            idx = nums.index(nums[mid])
+            cnt = nums.count(nums[mid])-1
+            return [base+idx,base+idx+cnt]
