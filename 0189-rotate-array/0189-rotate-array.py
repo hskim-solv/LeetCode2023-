1be9c5e2b2dd86891a0296ea7373e4ef3f4a5ef5
hyeongseop kim <times21c@gmail.com>
2023-07-26 07:45:30 +0900

    Time: 215 ms (87.08%), Space: 28.1 MB (11.91%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,6 +3,11 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-
+        """
         for _ in range(k % len(nums)):
             nums.insert(0,nums.pop())
+        """
+        
+        k = len(nums) - k % len(nums)
+        nums.extend(nums[:k])
+        del nums[:k]
