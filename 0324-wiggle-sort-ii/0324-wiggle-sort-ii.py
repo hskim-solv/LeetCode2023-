f8f30248dd6343b2989812940e0e7517d0345395
hyeongseop kim <times21c@gmail.com>
2023-07-28 02:40:51 +0900

    Time: 139 ms (100.00%), Space: 19.3 MB (70.66%) - LeetHub

--- 0324-wiggle-sort-ii/0324-wiggle-sort-ii.py
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.py
@@ -4,8 +4,8 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         nums.sort()
-        #half = len(nums[::2])
-        nums[::2], nums[1::2] = nums[:len(nums[::2])][::-1], nums[len(nums[::2]):][::-1]
+        half = len(nums[::2])
+        nums[::2], nums[1::2] = nums[:half][::-1], nums[half:][::-1]
                 
 
             
