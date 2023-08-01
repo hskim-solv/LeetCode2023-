b9dc6a5e7938984232c3e8a5ab3927e34389754c
hyeongseop kim <times21c@gmail.com>
2023-08-01 09:36:27 +0900

    Time: 840 ms (10.99%), Space: 16.3 MB (82.10%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.py
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.py
@@ -10,19 +10,19 @@ class Solution:
                 return p2[0],inf
             a = (p2[1]-p1[1])/(p2[0]-p1[0])
             return a, p1[1]-a*p1[0]
-        res =0
+        res = 0
         for i in range(len(points)-2):
-            d = defaultdict(int)
+            #d = defaultdict(int)
             for j in range(i+1,len(points)-1):
+                d = defaultdict(int)
                 a, b = s(points[i],points[j])
-                if (a,b) in d:
-                    continue
+
                 for k in range(j+1,len(points)):
                     xxx,yyy = points[k]
                     if (a == inf and b==yyy) or (a == xxx and b == inf) or math.isclose(yyy, b+a*xxx):
                         d[(a,b)] += 1
             
-            if d:
-                res = max(res,*d.values())
+                if d:
+                    res = max(res,*d.values())
         return res + 2
      
