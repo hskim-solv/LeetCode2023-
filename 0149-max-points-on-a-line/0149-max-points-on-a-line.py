46bfdb58899ba159fe6e4a740b25218b7b1d8280
hyeongseop kim <times21c@gmail.com>
2023-08-01 12:20:06 +0900

    Time: 71 ms (97.12%), Space: 16.3 MB (97.38%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.py
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.py
@@ -3,27 +3,17 @@ class Solution:
         if len(points) <= 2:
             return len(points)
         
-        def s(p1,p2):
-            if p2[1]==p1[1]:
-                return inf, p2[1]
-            elif p2[0]==p1[0]:
-                return p2[0],inf
-            a = (p2[1]-p1[1])/(p2[0]-p1[0])
-            return a, p1[1]-a*p1[0]
+        def slope(p1,p2):
+            if p2[0]==p1[0]:
+                return inf
+            return (p2[1]-p1[1])/(p2[0]-p1[0])
         res = 0
-        for i in range(len(points)-2):
+        
+        for i in range(len(points)):
             d = defaultdict(int)
-            for j in range(i+1,len(points)-1):
-                #d = defaultdict(int)
-                a, b = s(points[i],points[j])
-                if (a,b) in d:
-                    continue
-                for k in range(j+1,len(points)):
-                    xxx,yyy = points[k]
-                    if (a == inf and b==yyy) or (a == xxx and b == inf) or math.isclose(yyy, b+a*xxx):
-                        d[(a,b)] += 1
-            
+            for j in range(i+1,len(points)):
+                d[slope(points[i],points[j])] += 1
             if d:
                 res = max(res,*d.values())
-        return res + 2
+        return res + 1
      
