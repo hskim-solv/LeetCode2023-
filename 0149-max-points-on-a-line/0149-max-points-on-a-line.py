8de0065dfa84e9ca49fd31353aca1aa3de58b45a
hyeongseop kim <times21c@gmail.com>
2023-08-01 09:35:07 +0900

    Time: 820 ms (11.09%), Space: 16.2 MB (97.38%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.py
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.py
@@ -10,9 +10,10 @@ class Solution:
                 return p2[0],inf
             a = (p2[1]-p1[1])/(p2[0]-p1[0])
             return a, p1[1]-a*p1[0]
-        
+        res =0
         d = defaultdict(int)
         for i in range(len(points)-2):
+            d = defaultdict(int)
             for j in range(i+1,len(points)-1):
                 a, b = s(points[i],points[j])
                 if (a,b) in d:
@@ -21,8 +22,8 @@ class Solution:
                     xxx,yyy = points[k]
                     if (a == inf and b==yyy) or (a == xxx and b == inf) or math.isclose(yyy, b+a*xxx):
                         d[(a,b)] += 1
-
-        if d:
-            return max(d.values())+2
-        return 2
+            
+            if d:
+                res = max(res,*d.values())
+        return res + 2
      
