1cb56c432af3adf99995b7cfee174e3047681da6
hyeongseop kim <times21c@gmail.com>
2023-08-03 02:30:49 +0900

    Time: 229 ms (5.24%), Space: 16.8 MB (64.23%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.py
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.py
@@ -3,7 +3,6 @@ class Solution:
         for m in range(len(mat)-1):
             for n in range(len(mat[0])-1):
                 for i in range(min(n+1,m+1)):
-
                     if mat[m-i][n-i] > mat[m+1-i][n+1-i]:
                         mat[m-i][n-i], mat[m+1-i][n+1-i] =  mat[m+1-i][n+1-i], mat[m-i][n-i]
         return mat
