c0a0871768fb8855e8da387cc79476fe63b8d11d
hyeongseop kim <times21c@gmail.com>
2023-08-03 02:20:55 +0900

    Time: 238 ms (5.24%), Space: 16.5 MB (99.44%) - LeetHub

---
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.py
@@ -0,0 +1,10 @@
+class Solution:
+    def diagonalSort(self, mat: List[List[int]]) -> List[List[int]]:
+        for m in range(len(mat)-1):
+            for n in range(len(mat[0])-1):
+                for i in range(min(n+1,m+1)):
+
+                    if mat[m-i][n-i] > mat[m+1-i][n+1-i]:
+                        mat[m-i][n-i], mat[m+1-i][n+1-i] =  mat[m+1-i][n+1-i], mat[m-i][n-i]
+        return mat
+            
