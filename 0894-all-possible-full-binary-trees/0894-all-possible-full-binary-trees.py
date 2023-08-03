4f35ce66161521784c9b36f890a15f73976ddf0f
hyeongseop kim <times21c@gmail.com>
2023-08-03 20:28:51 +0900

    Time: 389 ms (5.01%), Space: 36.3 MB (5.75%) - LeetHub

--- 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.py
+++ 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.py
@@ -19,15 +19,14 @@ class Solution:
       right_branch = self.allPossibleFBT(n - i - 1)
       for left_count, left in enumerate(left_branch, 1):
         for right_count, right in enumerate(right_branch, 1):
-          tree = TreeNode()
+
           
-          # If we're using the last right branch, then this will be the last time this left branch is used and can hence
-          # be shallow copied, otherwise the tree will have to be cloned
-          tree.left = self.clone(left) if right_count < len(right_branch) else left
+          ret.append(
+            
+            TreeNode(
+          left=self.clone(left) if right_count < len(right_branch) else left,
+          right=self.clone(right) if left_count < len(left_branch) else right
+          )
           
-          # If we're using the last left branch, then this will be the last time this right branch is used and can hence
-          # be shallow copied, otherwise the tree will have to be cloned
-          tree.right = self.clone(right) if left_count < len(left_branch) else right
-          
-          ret.append(tree)
+          )
     return ret
