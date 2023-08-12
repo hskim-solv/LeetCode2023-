04069c7e87f9c97d423cb7629a4867099dc3bb4a
hyeongseop kim <times21c@gmail.com>
2023-08-12 16:16:01 +0900

    Time: 55 ms (52.05%), Space: 16.3 MB (71.49%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.py
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.py
@@ -1,3 +1,3 @@
 class Solution:
     def minOperations(self, n: int) -> int:
-        return sum( i*2 for i in range(1,(n+1)//2) ) + (n//2 if not n % 2 else 0)
+        return 2*sum( i for i in range(1,(n+1)//2) ) + (0 if n % 2 else n//2)
