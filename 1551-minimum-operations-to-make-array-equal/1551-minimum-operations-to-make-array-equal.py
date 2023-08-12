709c84736e2c032b7265d7ac00450a4d2241a52b
hyeongseop kim <times21c@gmail.com>
2023-08-12 16:00:52 +0900

    Time: 290 ms (5.18%), Space: 27.5 MB (5.18%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.py
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.py
@@ -2,8 +2,11 @@ class Solution:
     def __init__(self):
         self.cache = defaultdict(int)
         self.cache[1] = 0
-        self.cache[2] = 1
     def minOperations(self, n: int) -> int:
         if n not in self.cache:
-            self.cache[n] = n//2 + self.minOperations(n-1)
+            div, mod = divmod(n,2)
+            if not mod and not div:
+                self.cache[n-1] = div*2 + self.minOperations(n-2)
+            self.cache[n] = div + self.minOperations(n-1)
+
         return self.cache[n]
