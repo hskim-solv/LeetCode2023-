e892ab9d4035b9be2a5e0c6fd00404408db74651
hyeongseop kim <times21c@gmail.com>
2023-08-12 15:44:28 +0900

    Time: 185 ms (9.29%), Space: 26.9 MB (5.18%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.py
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.py
@@ -1,6 +1,7 @@
 class Solution:
-    @cache
+    def __init__(self):
+        self.cache = {1:0}
     def minOperations(self, n: int) -> int:
-        if n == 1:
-            return 0
+        if n in self.cache:
+            return self.cache[n]
         return n//2 + self.minOperations(n-1)
