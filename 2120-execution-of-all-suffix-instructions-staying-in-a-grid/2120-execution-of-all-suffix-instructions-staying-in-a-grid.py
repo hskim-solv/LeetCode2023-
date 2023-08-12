0080f695c420a8a7b7405fa1e47ba21ccb5a13b1
hyeongseop kim <times21c@gmail.com>
2023-08-12 12:51:00 +0900

    Time: 762 ms (91.95%), Space: 16.4 MB (66.53%) - LeetHub

---
+++ 2120-execution-of-all-suffix-instructions-staying-in-a-grid/2120-execution-of-all-suffix-instructions-staying-in-a-grid.py
@@ -0,0 +1,24 @@
+class Solution:
+    def executeInstructions(self, n: int, startPos: List[int], s: str) -> List[int]:
+        rnge = [-1, n]
+        res = []
+        
+        while s:
+            cnt = 0
+            y,x = startPos[:]
+            for c in s:
+                if c == "R":
+                    x += 1
+                elif c == "L":
+                    x -= 1
+                elif c == "U":
+                    y -= 1
+                else: # D
+                    y += 1
+                
+                if x in rnge or y in rnge:
+                    break
+                cnt += 1  
+            res.append(cnt)
+            s = s[1:]
+        return res
