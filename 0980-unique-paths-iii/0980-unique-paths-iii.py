ea3ad46bddbfbc8455e06036770d8d07deb2d778
hyeongseop kim <times21c@gmail.com>
2023-08-13 17:42:00 +0900

    Time: 57 ms (86.76%), Space: 16.4 MB (67.79%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.py
+++ 0980-unique-paths-iii/0980-unique-paths-iii.py
@@ -11,16 +11,17 @@ class Solution:
                     x0,y0 = i,j
         m -= 1
         n -= 1
-        res = []
+        res = [0]
         def dfs(path):
             x, y = path[-1]
             if grid[x][y] == -1:
                 return
             if grid[x][y] == 2:
                 if len(path)==cnt:
-                    res.append(1)
+                    res[0] += 1
                 else:
                     return
+            
             if x != 0:
                 if [x-1,y] not in path:
                     dfs(path + [ [x-1,y] ])
@@ -37,4 +38,4 @@ class Solution:
         
         dfs([[x0,y0]])
 
-        return sum(res)
+        return res[0]
