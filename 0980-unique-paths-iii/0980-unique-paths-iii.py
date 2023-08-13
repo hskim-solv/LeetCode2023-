1d0812b009c838e0850801c9472454b8a829e154
hyeongseop kim <times21c@gmail.com>
2023-08-13 17:50:38 +0900

    Time: 69 ms (59.15%), Space: 16.4 MB (33.22%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.py
+++ 0980-unique-paths-iii/0980-unique-paths-iii.py
@@ -9,31 +9,28 @@ class Solution:
                     cnt -= 1
                 elif grid[i][j] == 1:
                     x0,y0 = i,j
-        m -= 1
-        n -= 1
         res = [0]
         def dfs(path):
             x, y = path[-1]
-            if grid[x][y] == -1:
-                return
+            if not (0 <= x < m and 0 <= y < n and grid[x][y] >= 0): return
+            
             if grid[x][y] == 2:
                 if len(path)==cnt:
                     res[0] += 1
                 else:
                     return
             
-            if x != 0:
-                if [x-1,y] not in path:
-                    dfs(path + [ [x-1,y] ])
-            if y != 0:
-                if [x,y-1] not in path:
-                    dfs(path + [ [x,y-1] ])
-            if m != x:
-                if [x+1,y] not in path:
-                    dfs(path + [ [x+1,y] ])
-            if n != y:
-                if [x,y+1] not in path:
-                    dfs(path + [ [x,y+1] ])
+            if [x-1,y] not in path:
+                dfs(path + [ [x-1,y] ])
+            
+            if [x,y-1] not in path:
+                dfs(path + [ [x,y-1] ])
+            
+            if [x+1,y] not in path:
+                dfs(path + [ [x+1,y] ])
+
+            if [x,y+1] not in path:
+                dfs(path + [ [x,y+1] ])
             
         
         dfs([[x0,y0]])
