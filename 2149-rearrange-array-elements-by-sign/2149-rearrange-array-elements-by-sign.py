01ddd6ce6e0f0ed9f5df1e303328c21cee8dcd16
hyeongseop kim <times21c@gmail.com>
2023-08-14 18:36:35 +0900

    Time: 1221 ms (79.59%), Space: 44.8 MB (97.67%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -1,11 +1,9 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-
-        i = 0
         j = 0
         s1 = deque([])
         s2 = deque([])
-        while i < len(nums):
+        for i in range(len(nums)):
             if s1 and s2:
                 nums[j] = s1.popleft()
                 nums[j+1] = s2.popleft()
@@ -14,9 +12,7 @@ class Solution:
                 s1.append(nums[i])
             else:
                 s2.append(nums[i])
-
-            i+=1
-            #print(nums)
+            
         if s1 and s2:
             nums[j] = s1.popleft()
             nums[j+1] = s2.popleft()
