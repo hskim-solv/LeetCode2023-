656fbb820c6c713bd4a7e04bbfc034a8daae1aa0
hyeongseop kim <times21c@gmail.com>
2023-08-15 19:14:29 +0900

    Time: 1323 ms (95.59%), Space: 53.4 MB (22.97%) - LeetHub

--- 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.py
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.py
@@ -1,17 +1,16 @@
 class Solution:
     def onesMinusZeros(self, grid: List[List[int]]) -> List[List[int]]:
-        rz = []
+
         cz = []
         m = len(grid)
         n = len(grid[0])
-        for row in grid:
-            rz.append(row.count(0))
+
         for col in zip(*grid):
             cz.append(col.count(0))
         res = []
         for i in range(m):
             row = []
-            _rz = rz[i]
+            _rz = grid[i].count(0)
             _ro = n-_rz
             for j in range(n):
                 _cz = cz[j]
