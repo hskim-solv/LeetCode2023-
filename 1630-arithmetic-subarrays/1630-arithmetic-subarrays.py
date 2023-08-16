570dcc5f3bb3c77bc8d9804b6d106498e3a9a9f8
hyeongseop kim <times21c@gmail.com>
2023-08-16 17:19:11 +0900

    Time: 163 ms (88.22%), Space: 16.4 MB (98.80%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.py
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.py
@@ -4,14 +4,8 @@ class Solution:
         #diff = [n1-n0 for n1,n0 in zip(nums[:-1],nums[1:])]
         for i in range(len(l)):
   
-            e = set(nums[l[i]:r[i]+1])
-            if len(e) == 1:
-                res.append(True)
-                continue
-            if len(e) != r[i]-l[i]+1:
-                res.append(False)
-                continue
-            e = sorted(e)
+
+            e = sorted(nums[l[i]:r[i]+1])
             top = e.pop()
             diff = top - e[-1]
             while e:
