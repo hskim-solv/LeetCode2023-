9b3939ddacf1844a6fc2eeafff6413df814ce354
hyeongseop kim <times21c@gmail.com>
2023-08-16 17:02:04 +0900

    Time: 256 ms (16.35%), Space: 16.7 MB (36.78%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.py
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.py
@@ -2,16 +2,31 @@ class Solution:
     def checkArithmeticSubarrays(self, nums: List[int], l: List[int], r: List[int]) -> List[bool]:
         res = []
         for i in range(len(l)):
-            e = sorted(nums[l[i]:r[i]+1])
-            f = e.pop()
-            diff = f - e[-1]
-            while e:
-                if diff != f-e[-1]:
-                    res.append(False)
-                    break
-                f = e.pop()
+            e = set(nums[l[i]:r[i]+1])
+            if len(e) == 1:
+                res.append(True)
+                continue
+            if len(e) != r[i]-l[i]+1:
+                print(e,nums[l[i]:r[i]+1],r[i]-l[i])
+                res.append(False)
+                continue
+            mx, mn = max(e), min(e)
+            #print(mx-mn,len(e))
+            diff, mod = divmod(mx-mn,r[i]-l[i])
+            if mod:
+                #print('mod')
+                res.append(False)
+                continue
+            while mn in e:
+                print(mn,e)
+                e.remove(mn)
+                mn += diff
+            
+
             if not e:
                 res.append(True)
+            else:
+                res.append(False)
 
         return res
                     
