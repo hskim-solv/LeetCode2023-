acfcb6839dce5668f7d56b15cb76ac45447ccc70
hyeongseop kim <times21c@gmail.com>
2023-08-18 08:50:31 +0900

    Time: 55 ms (89.63%), Space: 18 MB (49.85%) - LeetHub

--- 1079-letter-tile-possibilities/1079-letter-tile-possibilities.py
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.py
@@ -1,9 +1,5 @@
 class Solution:
     def numTilePossibilities(self, tiles: str) -> int:
-        res = 0
-        for i in range(1,len(tiles)+1):
-            box = []
-            for j in permutations(tiles, i):
-                box.append(j)
-            res += len(set(box))
-        return res
+        
+        
+        return  sum([ len(set(permutations(tiles, i)))  for i in range(1,len(tiles)+1) ]) 
