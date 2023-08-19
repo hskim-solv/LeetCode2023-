16096c26bc01711e1dcecc9a746b1b6dea2f61cd
hyeongseop kim <times21c@gmail.com>
2023-08-19 20:59:52 +0900

    Time: 580 ms (79.56%), Space: 27.1 MB (16.06%) - LeetHub

---
+++ 1253-reconstruct-a-2-row-binary-matrix/1253-reconstruct-a-2-row-binary-matrix.py
@@ -0,0 +1,31 @@
+class Solution:
+    def reconstructMatrix(self, upper: int, lower: int, colsum: List[int]) -> List[List[int]]:
+        if sum(colsum) != upper+lower:
+            return []
+        res = [[],[]]
+        m = colsum.count(2)
+        upper -= m
+        lower -= m
+        for n in colsum:
+            if n == 2:
+                res[0].append(1)
+                res[1].append(1)
+            elif n == 0:
+                res[0].append(0)
+                res[1].append(0)
+            else:
+                print(upper,lower)
+
+                if upper>0:
+                    upper -= 1
+                    res[0].append(1)
+                    res[1].append(0)
+                elif lower > 0:
+                    lower -=1
+                    res[0].append(0)
+                    res[1].append(1)
+
+        if upper != 0 or lower !=0:
+            return []
+
+        return res
