91bc33e90f9ad5117240b2af8d581b1d15a375a8
hyeongseop kim <times21c@gmail.com>
2023-08-22 19:35:43 +0900

    Time: 41 ms (99.57%), Space: 18.1 MB (5.55%) - LeetHub

--- 1529-minimum-suffix-flips/1529-minimum-suffix-flips.py
+++ 1529-minimum-suffix-flips/1529-minimum-suffix-flips.py
@@ -1,3 +1,5 @@
 class Solution:
     def minFlips(self, target: str) -> int:
-        return max(0,len(list(filter(None, target.split('0'))))*2) - int(target[-1])
+        if '1' not in target:
+            return 0
+        return len(list(filter(None, target.split('0'))))*2 - int(target[-1])
