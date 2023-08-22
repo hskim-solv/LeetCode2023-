fb46acd3947cab1dbc2093f04c0eca3cfca7e7ac
hyeongseop kim <times21c@gmail.com>
2023-08-22 18:33:49 +0900

    Time: 49 ms (71.01%), Space: 16.2 MB (97.06%) - LeetHub

--- 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.py
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.py
@@ -3,6 +3,6 @@ class Solution:
         M, N = len(grid), len(grid[0])
         res = (1 << N - 1) * M
         for j in range(1, N):
-            cur = sum(row[j] == row[0] for row in grid)
+            cur = sum(grid[i][j] == grid[i][0] for i in range(M))
             res += max(cur, M - cur) * (1 << N - 1 - j)
         return res
