28a1ec19a6beafdca443506aecebd718b45fb4fa
hyeongseop kim <times21c@gmail.com>
2023-08-25 12:45:28 +0900

    Time: 489 ms (77.33%), Space: 20.2 MB (54.87%) - LeetHub

--- 0313-super-ugly-number/0313-super-ugly-number.py
+++ 0313-super-ugly-number/0313-super-ugly-number.py
@@ -1,14 +1,14 @@
 class Solution:
     def nthSuperUglyNumber(self, n: int, primes: List[int]) -> int:
-        ugly, dp, idx, ugly_nums = 1, [1], [0] * len(primes), [1] * len(primes)
+        size = len(primes)
+        dp, idx, ugly_nums = [1], [0] * size, [1] * size
         for i in range(1, n):
-            while ugly in ugly_nums:
-                j = ugly_nums.index(ugly)
+            #print(dp, idx, ugly_nums)
+            while dp[-1] in ugly_nums:
+                j = ugly_nums.index(dp[-1])
                 ugly_nums[j] = dp[idx[j]] * primes[j]
                 idx[j] += 1
-            # get the minimum
-            ugly = min(ugly_nums)
-            dp.append(ugly)
+            dp.append(min(ugly_nums))
         return dp[-1]
         
             
