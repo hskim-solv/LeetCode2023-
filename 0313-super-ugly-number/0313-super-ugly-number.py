1e424573bc97b0516c567d137a163d211a0fadb5
hyeongseop kim <times21c@gmail.com>
2023-08-25 12:27:14 +0900

    Time: 481 ms (77.75%), Space: 20.3 MB (54.87%) - LeetHub

--- 0313-super-ugly-number/0313-super-ugly-number.py
+++ 0313-super-ugly-number/0313-super-ugly-number.py
@@ -3,10 +3,10 @@ class Solution:
         size = len(primes)
         ugly, dp, idx, ugly_nums = 1, [1], [0] * size, [1] * size
         for i in range(1, n):
-            for j in range(size):
-                if ugly_nums[j] == ugly:
-                    ugly_nums[j] = dp[idx[j]] * primes[j]
-                    idx[j] += 1
+            while ugly in ugly_nums:
+                j = ugly_nums.index(ugly)
+                ugly_nums[j] = dp[idx[j]] * primes[j]
+                idx[j] += 1
             # get the minimum
             ugly = min(ugly_nums)
             dp.append(ugly)
