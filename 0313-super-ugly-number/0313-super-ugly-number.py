c5e47e4114b615859280aaed6a51c35e45ca4c61
hyeongseop kim <times21c@gmail.com>
2023-08-25 12:55:09 +0900

    Time: 1312 ms (54.45%), Space: 21.4 MB (39.41%) - LeetHub

--- 0313-super-ugly-number/0313-super-ugly-number.py
+++ 0313-super-ugly-number/0313-super-ugly-number.py
@@ -1,9 +1,8 @@
 class Solution:
     def nthSuperUglyNumber(self, n: int, primes: List[int]) -> int:
         size = len(primes)
-        dp, idx, ugly_nums = deque([1]), [0] * size, [1] * size
+        dp, idx, ugly_nums = [1], [0] * size, [1] * size
         for i in range(1, n):
-            #print(dp, idx, ugly_nums)
             while dp[-1] in ugly_nums:
                 j = ugly_nums.index(dp[-1])
                 ugly_nums[j] = dp[idx[j]] * primes[j]
@@ -11,8 +10,7 @@ class Solution:
             dp.append(min(ugly_nums))
             rest = min(idx)
             if rest > 0:
-                for _ in range(rest):
-                    dp.popleft()
+                dp = dp[rest:]
                 for i in range(size):
                     idx[i] -= rest
         return dp[-1]
