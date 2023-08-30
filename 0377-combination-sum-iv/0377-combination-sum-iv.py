520df72a642dbc53c2cf7d87444641d985604ff2
hyeongseop kim <times21c@gmail.com>
2023-08-30 12:01:36 +0900

    Time: 56 ms (29.91%), Space: 16.4 MB (34.93%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.py
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -6,7 +6,6 @@ class Solution:
         if target < 0:
             return 0
         for n in nums[::-1]:
-            
             if target-n not in self.d:
                 self.d[target-n] = self.combinationSum4(nums,target-n)
             self.d[target] += self.d[target-n]
