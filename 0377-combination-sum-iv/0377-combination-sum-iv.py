76329b74da8288d03276a46cf1d714cd7eccbb1c
hyeongseop kim <times21c@gmail.com>
2023-08-30 11:28:44 +0900

    Time: 39 ms (93.44%), Space: 16.2 MB (84.81%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.py
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -3,9 +3,10 @@ class Solution:
         self.d = defaultdict(int)
         self.d[0] = 1
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        if target < 0:
-            return 0
-        for n in nums:
+        
+        for n in nums[::-1]:
+            if target-n < 0:
+                continue
             if target-n not in self.d:
                 self.d[target-n] = self.combinationSum4(nums,target-n)
             self.d[target] += self.d[target-n]
