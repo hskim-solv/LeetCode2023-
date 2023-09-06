6f21e2e98449b95c720776d42a0ffadbe76bcfc3
hyeongseop kim <times21c@gmail.com>
2023-09-06 20:05:32 +0900

    Time: 948 ms (89.82%), Space: 69 MB (22.98%) - LeetHub

--- 2326-spiral-matrix-iv/2326-spiral-matrix-iv.py
+++ 2326-spiral-matrix-iv/2326-spiral-matrix-iv.py
@@ -5,38 +5,37 @@
 #         self.next = next
 class Solution:
     def spiralMatrix(self, m: int, n: int, head: Optional[ListNode]) -> List[List[int]]:
-        
         res = [[-1 for _ in range(n)] for _ in range(m)]
-        
-        lrdu = [0, n-1, 0, m-1]
+        z=0
+        druf = [0, n-1, m-1, 0]
         while True:
-            for x, y, z in ((0, 1, 2),(2, 3, 1),(1, 0, 3),(3, 2, 0)):
-                if x > y:
-                    rng = range(lrdu[x], lrdu[y]-1, -1)
+            for x, y in ((3, 1),(0, 2),(1, 3),(2, 0)):
+                if druf[x] > druf[y]:
+                    rng = range(druf[x], druf[y]-1, -1)
                 else:
-                    rng = range(lrdu[x], lrdu[y]+1)
-                if x in (0, 1):
+                    rng = range(druf[x], druf[y]+1)
+                if x in (3, 1):
                     for i in rng:
                         if head:
-                            res[lrdu[z]][i] = head.val
+                            res[druf[z]][i] = head.val
                             head = head.next
                         else:
                             break
-                elif x in (2, 3):
+                else:
+                    
                     for i in rng:
                         if head:
-                            res[i][lrdu[z]] = head.val
+                            res[i][druf[z]] = head.val
                             head = head.next
                         else:
                             break
-
-                if z in (0,2):
-                    lrdu[z] += 1
-                else:
-                    lrdu[z] -= 1 
-                #print(lrdu)
-                if (lrdu[2] > lrdu[3]) or (lrdu[0] > lrdu[1]):
-                    return res
                 
-
+                if z in (0,3):
+                    druf[z] += 1
+                else:
+                    druf[z] -= 1 
+                z += 1
+                z %= 4
+                if (druf[0] > druf[2]) or (druf[3] > druf[1]):
+                    return res
         return res
