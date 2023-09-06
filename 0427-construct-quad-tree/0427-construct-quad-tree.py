903c0286321ed6d080edc3657a0369b0c4d53eb8
hyeongseop kim <times21c@gmail.com>
2023-09-06 23:15:26 +0900

    Time: 99 ms (89.22%), Space: 17.2 MB (82.28%) - LeetHub

--- 0427-construct-quad-tree/0427-construct-quad-tree.py
+++ 0427-construct-quad-tree/0427-construct-quad-tree.py
@@ -13,19 +13,20 @@ class Node:
 class Solution:
     def construct(self, grid: List[List[int]]) -> 'Node':
         # 그리드가 전부 같은 값인지 판별
-        if len(grid)==1:
+        if len(grid) == 1:
             return Node(val=grid[0][0], isLeaf=1)
         stack = []
         for row in grid:
             stack = list(set(stack+row))
             if len(stack) != 1:
                 n = len(grid)
+                half = n//2
                 return Node(val=1, 
                             isLeaf=0,
-                           topLeft=self.construct([grid[i][:n//2] for i in range(n//2)]),
-                           topRight=self.construct([grid[i][n//2:] for i in range(n//2)]),
-                           bottomLeft=self.construct([grid[i][:n//2] for i in range(n//2, n)]),
-                           bottomRight=self.construct([grid[i][n//2:] for i in range(n//2, n)]),
+                           topLeft=self.construct([grid[i][:half] for i in range(half)]),
+                           topRight=self.construct([grid[i][half:] for i in range(half)]),
+                           bottomLeft=self.construct([grid[i][:half] for i in range(half, n)]),
+                           bottomRight=self.construct([grid[i][half:] for i in range(half, n)]),
                            )
         return Node(val=stack[0], isLeaf=1)
 
