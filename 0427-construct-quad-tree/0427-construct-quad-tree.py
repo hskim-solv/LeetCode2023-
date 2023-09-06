ab21e0ec776d6e10a0ea26ce485ec9fb30f711fd
hyeongseop kim <times21c@gmail.com>
2023-09-06 23:12:26 +0900

    Time: 111 ms (60.22%), Space: 17.1 MB (82.28%) - LeetHub

--- 0427-construct-quad-tree/0427-construct-quad-tree.py
+++ 0427-construct-quad-tree/0427-construct-quad-tree.py
@@ -22,8 +22,8 @@ class Solution:
                             isLeaf=0,
                            topLeft=self.construct([grid[i][:n//2] for i in range(n//2)]),
                            topRight=self.construct([grid[i][n//2:] for i in range(n//2)]),
-                           bottomLeft=self.construct([grid[i][:n//2] for i in range(n//2,n)]),
-                           bottomRight=self.construct([grid[i][n//2:] for i in range(n//2,n)]),
+                           bottomLeft=self.construct([grid[i][:n//2] for i in range(n//2, n)]),
+                           bottomRight=self.construct([grid[i][n//2:] for i in range(n//2, n)]),
                            )
         return Node(val=stack[0], isLeaf=1)
 
