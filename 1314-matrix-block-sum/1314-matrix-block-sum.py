1c9b70f4ea154c9aa27aafc82050130d15e610c6
hyeongseop kim <times21c@gmail.com>
2023-09-06 10:59:10 +0900

    Time: 1519 ms (23.41%), Space: 17.3 MB (90.82%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.py
+++ 1314-matrix-block-sum/1314-matrix-block-sum.py
@@ -8,8 +8,7 @@ class Solution:
                 for j in range(n):
                     box = 0
                     for r in range(max(0,i-k),min(m,i+k+1)):
-                        for c in range(max(0,j-k),min(n,j+k+1)):
-                            box += mat[r][c]
+                        box += sum(mat[r][max(0,j-k):min(n,j+k+1)])
                     row.append(box)
                 answer.append(row)
             return answer
