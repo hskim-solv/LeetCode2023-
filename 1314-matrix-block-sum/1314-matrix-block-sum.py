9e7ab8aaf5b8b25ed04a972135b9cb872297e779
hyeongseop kim <times21c@gmail.com>
2023-09-06 13:35:57 +0900

    Time: 88 ms (91.77%), Space: 17.3 MB (90.82%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.py
+++ 1314-matrix-block-sum/1314-matrix-block-sum.py
@@ -17,19 +17,19 @@ class Solution:
         # compute block sum by looking-up integral image
         output_image = [ [ 0 for _ in range(w) ] for _ in range(h) ]
         for i in range(h):
-            min_row, max_row = max(0, i-k), min(h-1, i+k)
             for j in range(w):
+                min_row, max_row = max(0, i-k), min(h-1, i+k)
                 min_col, max_col = max(0, j-k), min(w-1, j+k)
+               
                 output_image[i][j] = integral_image[max_row][max_col]
-                
                 if min_row > 0:
                     output_image[i][j] -= integral_image[min_row-1][max_col]
-                    if min_col > 0:
-                        output_image[i][j] += integral_image[min_row-1][min_col-1]
+                
                 if min_col > 0:
                     output_image[i][j] -= integral_image[max_row][min_col-1]
                     
-                
+                if min_col > 0 and min_row > 0:
+                    output_image[i][j] += integral_image[min_row-1][min_col-1]
                 
         return output_image
         
