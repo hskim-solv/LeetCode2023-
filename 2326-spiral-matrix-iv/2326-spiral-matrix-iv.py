64b2a74b58b0f15f5becb83fad7ad8052574504f
hyeongseop kim <times21c@gmail.com>
2023-09-06 22:29:37 +0900

    Time: 1025 ms (57.70%), Space: 68.7 MB (36.03%) - LeetHub

--- 2326-spiral-matrix-iv/2326-spiral-matrix-iv.py
+++ 2326-spiral-matrix-iv/2326-spiral-matrix-iv.py
@@ -6,7 +6,6 @@
 class Solution:
     def spiralMatrix(self, m: int, n: int, head: Optional[ListNode]) -> List[List[int]]:
         res = [[-1 for _ in range(n)] for _ in range(m)]
-        z=0
         druf = [0, n-1, m-1, 0]
         def rnge(x,y):
             if x > y:
@@ -20,17 +19,14 @@ class Solution:
                 res[i][z] = head.val
                 return head.next
         while head:
-            for x, y in ((3, 1),(0, 2),(1, 3),(2, 0)):
+            for x, y, z in ((3, 1, 0),(0, 2, 1),(1, 3, 2),(2, 0, 3)):
                 for i in rnge(druf[x], druf[y]):
                     if head:
                         head = rowcol(x,druf[z],i,head)
                     else:
                         return res
-
                 if z in (0,3):
                     druf[z] += 1
                 else:
                     druf[z] -= 1 
-                z += 1
-                z %= 4
         return res
