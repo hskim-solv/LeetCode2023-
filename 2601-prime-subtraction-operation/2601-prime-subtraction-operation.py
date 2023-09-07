b0d2bb5cac674108efa918945227e914a1d68bf3
hyeongseop kim <times21c@gmail.com>
2023-09-07 21:28:35 +0900

    Time: 118 ms (87.65%), Space: 16.6 MB (44.44%) - LeetHub

--- 2601-prime-subtraction-operation/2601-prime-subtraction-operation.py
+++ 2601-prime-subtraction-operation/2601-prime-subtraction-operation.py
@@ -2,15 +2,13 @@ class Solution:
     def primeSubOperation(self, nums: List[int]) -> bool:
         nums = nums[::-1]
         def is_prime(n):
-            if n < 2:
-                return False
             for i in range(2,n):
                 if (n % i) == 0:
                     return False
             return True
         for i in range(1, len(nums)):
             if nums[i-1] <= nums[i]:
-                for p in range(nums[i]-nums[i-1]+1, nums[i]):
+                for p in range(max(2,nums[i]-nums[i-1]+1), nums[i]):
                     if is_prime(p):
                         nums[i] -= p
                         break
