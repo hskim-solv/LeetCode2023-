b59d3ad9611771017270ea047bc637ad658a8f06
hyeongseop kim <times21c@gmail.com>
2023-09-07 20:56:06 +0900

    Time: 49 ms (41.28%), Space: 16.5 MB (84.78%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,7 +1,6 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         res = {tuple(),tuple(nums)}
-        n = len(nums)
         for i in range(1,len(nums)):
             res.update(set(map(tuple, map(sorted, combinations(nums, i)))))
 
