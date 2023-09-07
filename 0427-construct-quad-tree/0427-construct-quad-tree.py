e308371b5863f5a92c75ebb7206aed7014af3543
hyeongseop kim <times21c@gmail.com>
2023-09-07 10:58:44 +0900

    Time: 98 ms (91.04%), Space: 17 MB (95.38%) - LeetHub

--- 0427-construct-quad-tree/0427-construct-quad-tree.py
+++ 0427-construct-quad-tree/0427-construct-quad-tree.py
@@ -14,11 +14,12 @@ class Solution:
     def construct(self, grid: List[List[int]]) -> 'Node':
         stack = set()
         l, r = [], []
-        half = len(grid)//2
-        for row in grid:
-            stack.update(row)
-            l.append(row[:half])
-            r.append(row[half:])
+        n = len(grid)
+        half = n//2
+        for i in range(n):
+            stack.update(grid[i])
+            l.append(grid[i][:half])
+            r.append(grid[i][half:])
             if len(stack) > 1:
                 
                 for i in range(len(l),half*2):
