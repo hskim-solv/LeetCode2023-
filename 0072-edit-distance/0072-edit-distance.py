dfd6f38ab6ed8f07834444c3228af91984783fcb
hyeongseop kim <times21c@gmail.com>
2023-09-08 13:22:45 +0900

    Time: 180 ms (12.71%), Space: 22.5 MB (10.64%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.py
@@ -0,0 +1,18 @@
+class Solution:
+    def __init__(self):
+        self.cache = defaultdict(int)
+    def minDistance(self, word1: str, word2: str) -> int:
+        if not word1 and not word2:
+            return 0
+        if not len(word1):
+            return len(word2)
+        if not len(word2):
+            return len(word1)
+        if word1[0] == word2[0]:
+            return self.minDistance(word1[1:], word2[1:])
+        if (word1, word2) not in self.cache:
+            insert = 1 + self.minDistance(word1, word2[1:])
+            delete = 1 + self.minDistance(word1[1:], word2)
+            replace = 1 + self.minDistance(word1[1:], word2[1:])
+            self.cache[(word1, word2)] = min(replace, delete, insert)
+        return self.cache[(word1,word2)]
