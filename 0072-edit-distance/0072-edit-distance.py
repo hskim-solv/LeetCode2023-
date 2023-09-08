3b3b24d93b29f3c1db00430de47186eb38ec59da
hyeongseop kim <times21c@gmail.com>
2023-09-08 13:25:31 +0900

    Time: 171 ms (16.94%), Space: 22.4 MB (10.64%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -4,9 +4,9 @@ class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         if not word1 and not word2:
             return 0
-        if not len(word1):
+        if not word1:
             return len(word2)
-        if not len(word2):
+        if not word2:
             return len(word1)
         if word1[0] == word2[0]:
             return self.minDistance(word1[1:], word2[1:])
