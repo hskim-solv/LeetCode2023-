d7540b874b1b9ce4969f07d0b3fb88a6f4193594
hyeongseop kim <times21c@gmail.com>
2023-09-08 13:46:10 +0900

    Time: 164 ms (21.00%), Space: 22.2 MB (10.97%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -2,6 +2,7 @@ class Solution:
     def __init__(self):
         self.cache = defaultdict(int)
     def minDistance(self, word1: str, word2: str) -> int:
+        #print(self.cache,word1,word2)
         if not word1 and not word2:
             return 0
         if not word1:
@@ -12,8 +13,9 @@ class Solution:
             return self.minDistance(word1[:-1], word2[:-1])
         if (word1, word2) not in self.cache:
             self.cache[(word1, word2)] = min(
+                            self.minDistance(word1[:-1], word2[:-1])+1,
                             self.minDistance(word1, word2[:-1])+1,
                             self.minDistance(word1[:-1], word2)+1, 
-                            self.minDistance(word1[:-1], word2[:-1])+1
+                            
                         )
         return self.cache[(word1,word2)]
