d4b1e37805c41df2ed503f5aa7d943f9bfada320
hyeongseop kim <times21c@gmail.com>
2023-09-12 20:07:17 +0900

    Time: 157 ms (53.29%), Space: 18.8 MB (75.99%) - LeetHub

--- 1433-check-if-a-string-can-break-another-string/1433-check-if-a-string-can-break-another-string.py
+++ 1433-check-if-a-string-can-break-another-string/1433-check-if-a-string-can-break-another-string.py
@@ -1,8 +1,11 @@
 class Solution:
     def checkIfCanBreak(self, s1: str, s2: str) -> bool:
-        res1 = []
-        res2 = []
+        res = set()
         for x, y in zip(sorted(s1), sorted(s2)):
-            res1.append(ord(x) >= ord(y))
-            res2.append(ord(x) <= ord(y))
-        return all(res1) or all(res2)
+            if ord(x) == ord(y):
+                continue
+            res.add(ord(x) > ord(y))
+            if len(res) == 2:
+                return False
+                
+        return True
