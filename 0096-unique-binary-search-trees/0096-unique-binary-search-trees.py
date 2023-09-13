5854410770e09f0c1eeb985cdb7024079d333c82
hyeongseop kim <times21c@gmail.com>
2023-09-13 22:55:44 +0900

    Time: 33 ms (87.31%), Space: 16.3 MB (27.36%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
@@ -1,8 +1,10 @@
 class Solution:
+
     @cache
     def numTrees(self, n: int) -> int:
         if n <= 1: return 1
         return sum(self.numTrees(i-1) * self.numTrees(n-i) for i in range(1, n+1))
+
     '''
     def numTrees(self, n: int) -> int:
         dp = [0]*(n+1)
