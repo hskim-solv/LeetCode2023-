e0b7f22e6d991cea45666363c0628ed09f0bf460
hyeongseop kim <times21c@gmail.com>
2023-09-13 22:53:39 +0900

    Time: 35 ms (80.11%), Space: 16.2 MB (90.45%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
@@ -1,3 +1,4 @@
 class Solution:
     def numTrees(self, n: int) -> int:
-        return int(prod((4*i+2) / (i+2) for i in range(n)))
+        return factorial(2*n) // (factorial(n)*factorial(n+1))
+        #return int(prod((4*i+2) / (i+2) for i in range(n)))
