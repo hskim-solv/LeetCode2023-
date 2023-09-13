bd48bebef84f9e49c2234551e3c5107897b6c179
hyeongseop kim <times21c@gmail.com>
2023-09-13 21:35:36 +0900

    Time: 941 ms (89.05%), Space: 16.7 MB (42.86%) - LeetHub

--- 2002-maximum-product-of-the-length-of-two-palindromic-subsequences/2002-maximum-product-of-the-length-of-two-palindromic-subsequences.py
+++ 2002-maximum-product-of-the-length-of-two-palindromic-subsequences/2002-maximum-product-of-the-length-of-two-palindromic-subsequences.py
@@ -14,10 +14,9 @@ class Solution:
             if arr[i][0] ** 2 <= result: 
                 break
             for j in range(i+1, len(arr)-i//2):
-                if arr[i][1] & arr[j][1]:
-                    continue
-                if arr[i][0] * arr[j][0] > result:
-                    result = arr[i][0] * arr[j][0]
-                    break
+                if not arr[i][1] & arr[j][1]:
+                    if arr[i][0] * arr[j][0] > result:
+                        result = arr[i][0] * arr[j][0]
+                        break
         
         return result
