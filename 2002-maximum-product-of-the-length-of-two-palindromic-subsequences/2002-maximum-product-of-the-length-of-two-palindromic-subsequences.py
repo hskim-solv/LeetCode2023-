fb7346f3b2400f77168153048266af5ae842ca4a
hyeongseop kim <times21c@gmail.com>
2023-09-13 22:20:53 +0900

    Time: 678 ms (90.00%), Space: 16.6 MB (62.86%) - LeetHub

--- 2002-maximum-product-of-the-length-of-two-palindromic-subsequences/2002-maximum-product-of-the-length-of-two-palindromic-subsequences.py
+++ 2002-maximum-product-of-the-length-of-two-palindromic-subsequences/2002-maximum-product-of-the-length-of-two-palindromic-subsequences.py
@@ -1,8 +1,11 @@
 class Solution:
     def maxProduct(self, s: str) -> int:
         def is_pal(l):
-            return l == l[::-1]
-        arr = sorted([mask for mask in range(1, 1<<len(s)) if is_pal([s[i] for i in range(len(s)) if mask & (1<<i)])], key=lambda x: bin(x).count('1'))
+            while len(l) > 1:
+                if l.popleft() != l.pop():
+                    return False
+            return True
+        arr = sorted([mask for mask in range(1, 1<<len(s)) if is_pal(deque([s[i] for i in range(len(s)) if mask & (1<<i)]))], key=lambda x: bin(x).count('1'))
         res = 1
         while arr:
             p = arr.pop()
