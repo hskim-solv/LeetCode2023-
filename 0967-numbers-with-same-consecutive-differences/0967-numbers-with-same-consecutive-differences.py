84309cd1b2305350e08f7dcdd39d0264225f2493
hyeongseop kim <times21c@gmail.com>
2023-09-13 19:23:26 +0900

    Time: 46 ms (72.33%), Space: 16.6 MB (81.65%) - LeetHub

---
+++ 0967-numbers-with-same-consecutive-differences/0967-numbers-with-same-consecutive-differences.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numsSameConsecDiff(self, n: int, k: int) -> List[int]:
+        if n == 2:
+            res = set()
+            for i in range(1, 10-k):
+                res.add(int(str(i)+str(i+k)))
+            for i in range(max(1, k), 10):
+                res.add(int(str(i)+str(i-k)))
+            return res
+        res = set()
+        for num in self.numsSameConsecDiff(n-1, k):
+            c = int(str(num)[-1])
+            if c+k < 10:
+                res.add(int(str(num)+str(c+k)))
+            if c-k >= 0:
+                res.add(int(str(num)+str(c-k)))
+        return res
