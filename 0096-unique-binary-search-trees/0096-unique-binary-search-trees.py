98b41ff50665b3d959cbd170966c80adfbf1c204
hyeongseop kim <times21c@gmail.com>
2023-09-13 22:54:48 +0900

    Time: 34 ms (83.00%), Space: 16.2 MB (90.45%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
@@ -3,6 +3,7 @@ class Solution:
     def numTrees(self, n: int) -> int:
         if n <= 1: return 1
         return sum(self.numTrees(i-1) * self.numTrees(n-i) for i in range(1, n+1))
+    '''
     def numTrees(self, n: int) -> int:
         dp = [0]*(n+1)
         dp[0], dp[1] = 1, 1
@@ -10,5 +11,6 @@ class Solution:
             for j in range(1, i+1):
                 dp[i] += dp[j-1] * dp[i-j]
         return dp[n]
+        '''
         #return factorial(2*n) // (factorial(n)*factorial(n+1))
         #return int(prod((4*i+2) / (i+2) for i in range(n)))
