ed39b5c470d042f7d2827b0d72784220d7abe7c2
hyeongseop kim <times21c@gmail.com>
2023-09-13 21:52:23 +0900

    Time: 884 ms (89.05%), Space: 16.4 MB (91.67%) - LeetHub

--- 2002-maximum-product-of-the-length-of-two-palindromic-subsequences/2002-maximum-product-of-the-length-of-two-palindromic-subsequences.py
+++ 2002-maximum-product-of-the-length-of-two-palindromic-subsequences/2002-maximum-product-of-the-length-of-two-palindromic-subsequences.py
@@ -2,17 +2,16 @@ class Solution:
     def maxProduct(self, s: str) -> int:
         def is_pal(l):
             return l == l[::-1]
-        arr = sorted([mask for mask in range(1, 1<<len(s)) if is_pal([s[i] for i in range(len(s)) if mask & (1<<i)])],key=lambda x: bin(x).count('1'), reverse=True)
+        arr = sorted([mask for mask in range(1, 1<<len(s)) if is_pal([s[i] for i in range(len(s)) if mask & (1<<i)])], key=lambda x: bin(x).count('1'), reverse=True)
 
 
         result = 1
         for i in range(len(arr)):
-            len1 = bin(arr[i]).count('1')
-            if len1 ** 2 <= result: 
+            if bin(arr[i]).count('1') ** 2 <= result: 
                 break
             for j in range(i+1, len(arr)):
                 if not arr[i] & arr[j]:
-                    if len1 * bin(arr[j]).count('1') > result:
-                        result = len1 * bin(arr[j]).count('1')
+                    if bin(arr[i]).count('1') * bin(arr[j]).count('1') > result:
+                        result = bin(arr[i]).count('1') * bin(arr[j]).count('1')
                         break
         return result
