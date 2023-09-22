4edf069db064e2b4f5752f8f9126ab840b56252a
hyeongseop kim <times21c@gmail.com>
2023-09-22 13:33:52 +0900

    Time: 8285 ms (10.60%), Space: 63.7 MB (24.18%) - LeetHub

---
+++ 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.py
@@ -0,0 +1,40 @@
+
+class Solution:
+    def maximumSafenessFactor(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        thieves = [[i, j] for j in range(n) for i in range(n) if grid[i][j]]
+
+        visited = [[0 for j in range(n)] for i in range(n)]
+        distance = [[0 for j in range(n)] for i in range(n)]
+
+        # find the minimum mahatten distance of each cell to theives 
+        depth = 0
+        while thieves:
+            B = []
+            for i, j in thieves:
+                if visited[i][j]:
+                    continue
+                visited[i][j], distance[i][j] = 1, depth
+                for x, y in [[i+1, j], [i-1, j], [i, j+1], [i, j-1]]:
+                    if 0 <= x < n and 0 <= y < n:
+                        B.append([x, y])
+            thieves = B
+            depth += 1
+            
+        # start from 0,0 and use dijkstra  
+        from heapq import heappop, heappush
+        visited = [[0 for j in range(n)] for i in range(n)]
+        pq = [[-distance[0][0], 0, 0]]
+        while pq:
+            dis, i, j = heappop(pq)
+            if visited[i][j]:
+                continue
+            if i == n-1 and j == n-1:
+                return -dis
+            
+            visited[i][j] = 1
+
+            for x, y in [[i+1, j], [i-1, j], [i, j+1], [i, j-1]]:
+                if 0 <= x < n and 0 <= y < n:
+                    heappush(pq, [-min(-dis, distance[x][y]), x, y])
+        return -1
