63928c995b3464975b11c3612c78b48866b09ab0
hyeongseop kim <times21c@gmail.com>
2023-09-22 12:08:32 +0900

    Time: 68 ms (48.88%), Space: 16.2 MB (89.65%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.py
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.py
@@ -1,5 +1,6 @@
 class Solution:
     def buyChoco(self, prices: List[int], money: int) -> int:
         prices.sort(reverse=True)
-        return money if sum(prices[-2:]) > money else money-sum(prices[-2:])
+        total = sum(prices[-2:])
+        return money if total > money else money-total
             
