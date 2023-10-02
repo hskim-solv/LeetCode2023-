ff45f58291695a3fae3cf2a81a303b42719a1f05
hyeongseop kim <times21c@gmail.com>
2023-10-02 11:58:29 +0900

    Time: 642 ms (5.76%), Space: 16.3 MB (72.19%) - LeetHub

--- 2760-longest-even-odd-subarray-with-threshold/2760-longest-even-odd-subarray-with-threshold.py
+++ 2760-longest-even-odd-subarray-with-threshold/2760-longest-even-odd-subarray-with-threshold.py
@@ -5,17 +5,14 @@ class Solution:
         ln = len(nums)
         mx = 0
         for l in range(ln):
-            if nums[l] % 2 == 1:
+            if nums[l] % 2 == 1 or nums[l] > threshold: 
                 continue
-            if nums[l] > threshold:
-                continue
+
             for r in range(l+1, ln):
-                if nums[r] > threshold:
-                    break
-                if nums[r-1] % 2 == nums[r] % 2:
+                if nums[r] > threshold or nums[r-1] % 2 == nums[r] % 2: 
                     break
                 mx = max(mx, r+1-l)
-                print(r,l)
+                #print(r,l)
             if mx == 0:
                 mx = 1
         return mx
