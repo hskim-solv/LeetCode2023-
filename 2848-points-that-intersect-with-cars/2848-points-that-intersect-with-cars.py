b3b3f4f11138794c5a780fe2bc33570f980ff861
hyeongseop kim <times21c@gmail.com>
2023-10-02 17:34:23 +0900

    Time: 84 ms (52.92%), Space: 16.3 MB (75.82%) - LeetHub

--- 2848-points-that-intersect-with-cars/2848-points-that-intersect-with-cars.py
+++ 2848-points-that-intersect-with-cars/2848-points-that-intersect-with-cars.py
@@ -3,6 +3,5 @@ class Solution:
         res = set()
         for n in nums:
             for i in range(n[0],n[1]+1):
-                if i not in res:
-                    res.add(i)
+                res.add(i)
         return len(res)
