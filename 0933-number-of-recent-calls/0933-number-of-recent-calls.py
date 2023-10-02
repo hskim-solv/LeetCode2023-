61a1c8cd7481b162332c4f745603a98bfd9885f2
hyeongseop kim <times21c@gmail.com>
2023-10-02 19:37:54 +0900

    Time: 232 ms (37.51%), Space: 21.6 MB (28.47%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.py
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.py
@@ -5,7 +5,7 @@ class RecentCounter:
 
     def ping(self, t: int) -> int:
         self.q.append(t)
-        while not(t-3000 <= self.q[0] <= t):
+        while not (t-3000 <= self.q[0] <= t):
             self.q.popleft()
         return len(self.q)
 
