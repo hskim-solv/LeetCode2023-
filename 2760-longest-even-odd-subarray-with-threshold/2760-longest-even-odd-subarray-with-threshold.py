2162c711bd37bd1b4c1d0fb2cf869018fff6156b
hyeongseop kim <times21c@gmail.com>
2023-10-02 12:18:29 +0900

    Time: 298 ms (48.74%), Space: 16.3 MB (39.04%) - LeetHub

--- 2760-longest-even-odd-subarray-with-threshold/2760-longest-even-odd-subarray-with-threshold.py
+++ 2760-longest-even-odd-subarray-with-threshold/2760-longest-even-odd-subarray-with-threshold.py
@@ -2,7 +2,6 @@ class Solution:
     def longestAlternatingSubarray(self, nums: List[int], threshold: int) -> int:
         n = len(nums)-1
         maxi = 0
-        i = 0
         j = -1
         flag = False
 
@@ -17,13 +16,13 @@ class Solution:
                 if (nums[j-1]+nums[j]) % 2:
                     maxi = max(maxi, j-i+1)
                 else:
-                    flag = 0
+                    flag = False
                     j -= 1
             else:
                 if nums[j] % 2 == 0:
                     i = j
                     maxi = max(maxi, j-i+1)
-                    flag = 1
+                    flag = True
 
 
             
