253ec4d052fa2c0d113cce333f49721dd16ff814
hyeongseop kim <times21c@gmail.com>
2023-10-02 12:01:03 +0900

    Time: 622 ms (7.73%), Space: 16.3 MB (39.04%) - LeetHub

--- 2760-longest-even-odd-subarray-with-threshold/2760-longest-even-odd-subarray-with-threshold.py
+++ 2760-longest-even-odd-subarray-with-threshold/2760-longest-even-odd-subarray-with-threshold.py
@@ -2,7 +2,7 @@ class Solution:
     def longestAlternatingSubarray(self, nums: List[int], threshold: int) -> int:
         mx = 0
         for l in range(len(nums)):
-            if nums[l] % 2 == 1 or nums[l] > threshold: 
+            if nums[l] % 2 or nums[l] > threshold: 
                 continue
             for r in range(l+1, len(nums)):
                 if nums[r] > threshold or nums[r-1] % 2 == nums[r] % 2: 
