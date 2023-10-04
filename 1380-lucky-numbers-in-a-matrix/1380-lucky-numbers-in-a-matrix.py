a2786bec027940e3c68d60eed0721dbefc0a5aab
hyeongseop kim <times21c@gmail.com>
2023-10-04 12:50:05 +0900

    Time: 124 ms (52.46%), Space: 16.7 MB (53.68%) - LeetHub

--- 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.py
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.py
@@ -1,11 +1,4 @@
 class Solution:
     def luckyNumbers (self, matrix: List[List[int]]) -> List[int]:
-        transposed = list(zip(*matrix))
-        res = []
-        
-        for row in matrix:
-            rmin = min(row)
-            j = row.index(rmin)
-            if max(transposed[j]) == rmin:
-                res.append(rmin)
-        return res
+
+        return [max(col) for col in zip(*matrix) if min(matrix[col.index(max(col))]) == max(col)]
