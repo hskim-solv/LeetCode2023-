07fc2801cfedc8f0232054384a962a2c9a99e49f
hyeongseop kim <times21c@gmail.com>
2023-12-30 21:25:43 +0900

    Time: 194 ms (5.01%), Space: 17.1 MB (11.91%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.py
+++ 0763-partition-labels/0763-partition-labels.py
@@ -1,9 +1,12 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
         res = []
+        cum = 0
         for i in range(1,len(s)+1):
 
             if not set(s[i:])&set(s[:i]):
-                res.append(i-sum(res))
+
+                res.append(i-cum)
+                cum += res[-1]
                 
         return res
