97671ee99eabff1cace992b04249c7b79c823db3
hyeongseop kim <times21c@gmail.com>
2023-12-30 21:31:23 +0900

    Time: 135 ms (5.01%), Space: 17.1 MB (11.91%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.py
+++ 0763-partition-labels/0763-partition-labels.py
@@ -3,9 +3,8 @@ class Solution:
         res = []
         cum = 0
         for i in range(1,len(s)+1):
-
-            if not set(s[i:])&set(s[:i]):
-
+            
+            if not set(s[i:])&set(s[cum:i]):
                 res.append(i-cum)
                 cum += res[-1]
                 
