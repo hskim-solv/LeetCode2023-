5d8e37a64c82d6ba8102321dc0749069ac84ae46
hyeongseop kim <times21c@gmail.com>
2024-01-01 21:38:05 +0900

    Time: 71 ms (7.08%), Space: 17.4 MB (9.07%) - LeetHub

--- 0999-available-captures-for-rook/0999-available-captures-for-rook.py
+++ 0999-available-captures-for-rook/0999-available-captures-for-rook.py
@@ -1,31 +1,30 @@
 class Solution:
     def numRookCaptures(self, board: List[List[str]]) -> int:
-
         ans = 0
         for i in range(8):
             for j in range(8):
                 if board[i][j] == 'R':
                     for left in range(j-1, -1, -1):
-                        if board[i][left] == 'B':
-                            break
+                        if board[i][left] == '.': continue
+                        if board[i][left] == 'B': break
                         if board[i][left] == 'p':
                             ans += 1
                             break
                     for right in range(j+1, 8):
-                        if board[i][right] == 'B':
-                            break
+                        if board[i][right] == '.': continue
+                        if board[i][right] == 'B': break
                         if board[i][right] == 'p':
                             ans += 1
                             break
                     for up in range(i-1, -1, -1):
-                        if board[up][j] == 'B':
-                            break
+                        if board[up][j] == '.': continue
+                        if board[up][j] == 'B': break
                         if board[up][j] == 'p':
                             ans += 1
                             break
                     for down in range(i+1, 8):
-                        if board[down][j] == 'B':
-                            break
+                        if board[down][j] == '.': continue
+                        if board[down][j] == 'B': break
                         if board[down][j] == 'p':
                             ans += 1
                             break
