95c8756d019c7af99fa7170eb6200a760c63bfae
hyeongseop kim <times21c@gmail.com>
2024-01-01 22:15:29 +0900

    Time: 147 ms (5.26%), Space: 17.4 MB (11.16%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.py
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -2,11 +2,10 @@ class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
         m, n = len(matrix), len(matrix[0])
 
-        for i in range(m-2,-1,-1):
-            if len({matrix[i+step][step] for step in range(min(m-i, n))}) != 1:
+        for i in range(m-2, -1, -1):
+            if len({matrix[i+step][step] for step in range(min(m-i, n))}) > 1:
                 return False
-        for j in range(n-2,0,-1):
-
-            if len({matrix[step][j+step] for step in range(min(n-j, m))}) != 1:
+        for j in range(n-2, 0, -1):
+            if len({matrix[step][j+step] for step in range(min(n-j, m))}) > 1:
                 return False
         return True
