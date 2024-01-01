0f4958baae520e13e1bfde2331388713109f22aa
hyeongseop kim <times21c@gmail.com>
2024-01-01 16:53:54 +0900

    Time: 253 ms (5.01%), Space: 19.1 MB (40.55%) - LeetHub

--- 1030-matrix-cells-in-distance-order/1030-matrix-cells-in-distance-order.py
+++ 1030-matrix-cells-in-distance-order/1030-matrix-cells-in-distance-order.py
@@ -1,9 +1,6 @@
 class Solution:
     def allCellsDistOrder(self, rows: int, cols: int, rCenter: int, cCenter: int) -> List[List[int]]:
-        d = {}
-        for i in range(rows):
-            for j in range(cols):
-                d[(i,j)] = abs(rCenter-i) + abs(cCenter-j)
 
-        return sorted(d.keys(),key=lambda x:d[x])
+
+        return sorted([(i,j) for i in range(rows) for j in range(cols)],key=lambda x:abs(rCenter-x[0]) + abs(cCenter-x[1]))
         
