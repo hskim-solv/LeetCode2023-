25f36f322608147d8f66eb3d5b7b7a0b2dfe36d7
hyeongseop kim <times21c@gmail.com>
2024-01-01 15:55:07 +0900

    Time: 109 ms (98.58%), Space: 17.9 MB (9.49%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.py
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.py
@@ -1,10 +1,5 @@
 class Solution:
     def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
-        res = list(range(1,len(grid)**2+1))
+        res = set(range(1,len(grid)**2+1))
         count_dict = Counter(chain(*grid))
-        for k,v in count_dict.items():
-            if v == 1:
-                res.remove(k)
-        if res[0] != count_dict.most_common(1)[0][0]:
-            res.reverse()
-        return res
+        return [count_dict.most_common(1)[0][0],*(res-set(count_dict.keys()))]
