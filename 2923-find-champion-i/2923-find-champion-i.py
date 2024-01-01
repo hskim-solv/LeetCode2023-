a8fdd97d6fa7cd81618e4fddbcc8c5ad4a3ef4b0
hyeongseop kim <times21c@gmail.com>
2024-01-01 16:29:18 +0900

    Time: 937 ms (5.12%), Space: 17.5 MB (5.00%) - LeetHub

--- 2923-find-champion-i/2923-find-champion-i.py
+++ 2923-find-champion-i/2923-find-champion-i.py
@@ -2,5 +2,5 @@ class Solution:
     def findChampion(self, grid: List[List[int]]) -> int:
         n = len(grid)
         for i in range(n):
-            if sum(grid[i]) == n-1:
+            if sum(grid[i])+1 == n:
                 return i
