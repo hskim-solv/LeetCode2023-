54e6e7f2645cfe9e53556192b7d110fa19882b49
hyeongseop kim <times21c@gmail.com>
2024-01-01 17:27:36 +0900

    Time: 64 ms (100.00%), Space: 17.6 MB (6.90%) - LeetHub

---
+++ 0892-surface-area-of-3d-shapes/0892-surface-area-of-3d-shapes.py
@@ -0,0 +1,21 @@
+class Solution:
+    def surfaceArea(self, grid: List[List[int]]) -> int:
+        n, m = len(grid),len(grid[0])
+        ans = 0
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j]:
+                    ans += 2
+                if i == 0:
+                    ans += grid[i][j]
+                if i == n-1:
+                    ans += grid[i][j]
+                else:
+                    ans += abs(grid[i][j]-grid[i+1][j])
+                if j == m-1:
+                    ans += grid[i][j]
+                else:
+                    ans += abs(grid[i][j]-grid[i][j+1])
+                if j == 0:
+                    ans += grid[i][j]
+        return ans
