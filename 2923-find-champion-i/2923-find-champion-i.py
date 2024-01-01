9036f208d9f97dcb3071a312cbbc169630f42359
hyeongseop kim <times21c@gmail.com>
2024-01-01 16:19:21 +0900

    Time: 982 ms (5.12%), Space: 17.7 MB (5.00%) - LeetHub

--- 2923-find-champion-i/2923-find-champion-i.py
+++ 2923-find-champion-i/2923-find-champion-i.py
@@ -1,4 +1,17 @@
 class Solution:
     def findChampion(self, grid: List[List[int]]) -> int:
-        cnt_victory = list(map(sum,grid))
-        return cnt_victory.index(max(cnt_victory))
+        vic_top = 0
+        top_team = 0
+        n = len(grid)
+        
+        for i in range(n):
+            cnt_vic = 0
+            for j in range(n):
+                if grid[i][j]:
+                    cnt_vic += 1
+                
+            if vic_top < cnt_vic:
+                vic_top = cnt_vic
+                top_team = i
+            #print(top_team,vic_top,cnt_vic)
+        return top_team
