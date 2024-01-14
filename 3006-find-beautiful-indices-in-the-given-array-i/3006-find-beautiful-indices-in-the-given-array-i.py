aeecd1153b7bbbe016380269b4eedcb70b525793
hyeongseop kim <times21c@gmail.com>
2024-01-14 13:21:41 +0900

    Time: 3042 ms (25.00%), Space: 19.7 MB (25.00%) - LeetHub

--- 3006-find-beautiful-indices-in-the-given-array-i/3006-find-beautiful-indices-in-the-given-array-i.py
+++ 3006-find-beautiful-indices-in-the-given-array-i/3006-find-beautiful-indices-in-the-given-array-i.py
@@ -2,16 +2,18 @@ class Solution:
     def beautifulIndices(self, s: str, a: str, b: str, k: int) -> List[int]:
         ans = []
         sa = s[:]
-
-
+        len_a = len(a)
+        len_b = len(b)
+        aglue = '*'*len_a
+        bglue = '*'*len_b
         while a in sa and b in s:
             idx1 = sa.index(a)
             idx2 = s.index(b)
             if abs(idx1-idx2) <= k:
                 ans.append(idx1)
-                sa = sa[:idx1] + '*'*len(a) + sa[idx1+len(a):]  
+                sa = sa[:idx1] + aglue + sa[idx1+len_a:] 
             elif idx1-idx2 > k :
-                s = s[:idx2] + '*'*len(b) + s[idx2+len(b):]
+                s = s[:idx2] + bglue + s[idx2+len_b:]
             elif idx2-idx1 > k :
-                sa = sa[:idx1] + '*'*len(a) + sa[idx1+len(a):]  
+                sa = sa[:idx1] + aglue + sa[idx1+len_a:]  
         return ans
