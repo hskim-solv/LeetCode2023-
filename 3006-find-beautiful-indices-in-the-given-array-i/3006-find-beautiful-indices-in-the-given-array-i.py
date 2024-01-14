8a6237b85887da1f1158893973ab1dd25e108645
hyeongseop kim <times21c@gmail.com>
2024-01-14 14:07:12 +0900

    Time: 173 ms (25.00%), Space: 19.8 MB (25.00%) - LeetHub

--- 3006-find-beautiful-indices-in-the-given-array-i/3006-find-beautiful-indices-in-the-given-array-i.py
+++ 3006-find-beautiful-indices-in-the-given-array-i/3006-find-beautiful-indices-in-the-given-array-i.py
@@ -10,18 +10,16 @@ class Solution:
         while a in sa and b in s:
             idx1 = sa.index(a)
             idx2 = s.index(b)
-            #print(abs((ia+idx1)-(ib+idx2) ))
-            if abs((ia+idx1)-(ib+idx2) ) <= k:
+            x = (ia+idx1)-(ib+idx2)
+            if abs(x) <= k:
                 sa = sa[idx1+len_a:] 
-                #print(sa)
                 ans.append(ia+idx1)
                 ia += idx1+len_a
                 
-            elif (ia+idx1)-(ib+idx2) > k :
+            elif x > k:
                 s = s[idx2+len_b:]
                 ib += idx2+len_b
-            elif (ib+idx2)-(ia+idx1) > k :
+            elif -x > k:
                 sa = sa[idx1+len_a:]  
                 ia += idx1+len_a
-            #print(idx1,idx2,ia,ib,sa,s)
         return ans
