8efe2a8518685092ad5ba08066dd211f62d4d12d
hyeongseop kim <times21c@gmail.com>
2024-01-26 17:04:13 +0900

    Time: 46 ms (36.97%), Space: 16.7 MB (56.05%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -6,7 +6,7 @@ class Solution:
         
         while m and n:
             if nums1[m-1] < nums2[n-1]:
-                nums1[m+n-1] = nums2.pop()
+                nums1[m+n-1] = nums2[n-1]
                 n -= 1
             else:
                 nums1[m+n-1] = nums1[m-1]
