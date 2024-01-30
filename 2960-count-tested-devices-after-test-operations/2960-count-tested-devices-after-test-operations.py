1f52116bf7a77412ad04cd198b7f093802c42802
hyeongseop kim <times21c@gmail.com>
2024-01-30 11:51:08 +0900

    Time: 66 ms (58.52%), Space: 16.6 MB (16.66%) - LeetHub

--- 2960-count-tested-devices-after-test-operations/2960-count-tested-devices-after-test-operations.py
+++ 2960-count-tested-devices-after-test-operations/2960-count-tested-devices-after-test-operations.py
@@ -1,14 +1,12 @@
 class Solution:
     def countTestedDevices(self, batteryPercentages: List[int]) -> int:
-
-        """        
-        n = len(batteryPercentages)
+      
         ans = 0 
         for i in range(len(batteryPercentages)):
             if batteryPercentages[i]:
-                for j in range(1,n-i):
-                    if batteryPercentages[i+j]:
-                        batteryPercentages[i+j] -= 1
+                for j in range(i,len(batteryPercentages)):
+                    if batteryPercentages[j]:
+                        batteryPercentages[j] -= 1
                 ans += 1
         return ans
         """
@@ -16,3 +14,4 @@ class Solution:
         for Percentage in batteryPercentages:
             cnt += Percentage > cnt
         return cnt
+        """
