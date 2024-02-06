aabe76780e5dda500a201626768ae9641cbe22e3
hyeongseop kim <times21c@gmail.com>
2024-02-06 12:14:50 +0900

    Time: 94 ms (98.31%), Space: 17.7 MB (63.17%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,18 +1,14 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
         n = len(nums)-1
-        if n == 0:
-            return 0  
+        if n == 0: return 0  
         j = nums[0]
-        if j == n:
-            return 1
         i = 0
         cnt = 1
         while i+j < n:
             cnt += 1
-            if i+j < n:
-                i += 1
-                jumps = [i+s for i,s in enumerate(nums[i:i+j], 1)]
-                i += jumps.index(max(jumps))
+            i += 1
+            jumps = [i+s for i,s in enumerate(nums[i:i+j], 1)]
+            i += jumps.index(max(jumps))
             j = nums[i]
         return cnt
