ae2ce5dcc9151cccfd7d005277d85744b9a7654d
hyeongseop kim <times21c@gmail.com>
2024-03-08 16:20:59 +0900

    Time: 35 ms (72.69%), Space: 16.5 MB (62.04%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.py
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.py
@@ -1,10 +1,10 @@
 class Solution:
     def buildArray(self, target: List[int], n: int) -> List[str]:
-        f = target[-1]
+        f = target.pop()
         res = f*["Push"]
         f -= 1
-        while f != 0:
+        while f:
             if f not in target:
-                res.insert(f,"Pop")
+                res.insert(f, "Pop")
             f -= 1
         return res
