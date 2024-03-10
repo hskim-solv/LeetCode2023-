0fcada389b24c5149361338453e1eda718c07739
hyeongseop kim <times21c@gmail.com>
2024-03-10 15:56:36 +0900

    Time: 249 ms (5.21%), Space: 19.1 MB (39.41%) - LeetHub

---
+++ 1472-design-browser-history/1472-design-browser-history.py
@@ -0,0 +1,27 @@
+class BrowserHistory:
+
+    def __init__(self, homepage: str):
+        self.url_list = [homepage]
+        self.pos = 0
+
+    def visit(self, url: str) -> None:
+        self.url_list = self.url_list[:self.pos+1]
+        self.url_list.append(url)
+        self.pos = len(self.url_list)-1
+
+    def back(self, steps: int) -> str:
+        self.pos = max(0,self.pos-steps)
+        print(self.url_list,self.pos)
+        return self.url_list[self.pos]
+
+    def forward(self, steps: int) -> str:
+        self.pos = min(self.pos+steps,len(self.url_list)-1)
+        print(self.url_list,self.pos)
+        return self.url_list[self.pos]
+
+
+# Your BrowserHistory object will be instantiated and called as such:
+# obj = BrowserHistory(homepage)
+# obj.visit(url)
+# param_2 = obj.back(steps)
+# param_3 = obj.forward(steps)
