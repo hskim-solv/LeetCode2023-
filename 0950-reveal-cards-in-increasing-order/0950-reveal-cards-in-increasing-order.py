89ae4bcb28f29230980cfdd912d6202a3b0e6048
hyeongseop kim <times21c@gmail.com>
2024-03-10 13:36:16 +0900

    Time: 42 ms (79.68%), Space: 16.8 MB (84.76%) - LeetHub

--- 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.py
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.py
@@ -2,7 +2,7 @@ class Solution:
     def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
         deck.sort(reverse=True)
         result = deque()
-        result.appendleft(deck.pop(0))
+        result.append(deck.pop(0))
         for i, elem in enumerate(deck):
             result.appendleft(result.pop())
             result.appendleft(elem) 
