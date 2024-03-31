0fa3f455b0469f7ca7ae53a741096f4b473be024
hyeongseop kim <times21c@gmail.com>
2024-03-31 16:13:54 +0900

    Time: 43 ms (87.38%), Space: 16.4 MB (99.53%) - LeetHub

--- 2951-find-the-peaks/2951-find-the-peaks.py
+++ 2951-find-the-peaks/2951-find-the-peaks.py
@@ -1,5 +1,6 @@
 class Solution:
     def findPeaks(self, mountain: List[int]) -> List[int]:
+        '''
         length = len(mountain)
         res = []
         for i in range(1, length-1):
@@ -7,3 +8,5 @@ class Solution:
                 if mountain[i] > mountain[i+1]:
                     res.append(i)
         return res
+        '''
+        return [i for i in range(1, len(mountain)-1) if mountain[i-1] < mountain[i] and mountain[i] > mountain[i+1]]
