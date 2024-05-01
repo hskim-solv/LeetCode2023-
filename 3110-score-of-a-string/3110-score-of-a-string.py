90e09d9e09bf82dd5d493daec0e2304dafc861b7
hyeongseop kim <times21c@gmail.com>
2024-05-01 16:08:01 +0900

    Time: 36 ms (59.52%), Space: 16.6 MB (6.98%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.py
+++ 3110-score-of-a-string/3110-score-of-a-string.py
@@ -1,4 +1,5 @@
 class Solution:
     def scoreOfString(self, s: str) -> int:
-        return sum(abs(ord(x)-ord(y)) for x, y in pairwise(s))
+        return sum(map(lambda x: abs(ord(x[0])-ord(x[1])), pairwise(s)))
+        #return sum(abs(ord(x)-ord(y)) for x, y in pairwise(s))
             
