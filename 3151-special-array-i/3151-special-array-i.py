450bc1d9f5af529bb493b08c47780c8dfac3f4bd
hyeongseop kim <times21c@gmail.com>
2024-05-21 18:24:57 +0900

    Time: 54 ms (44.64%), Space: 16.5 MB (82.60%) - LeetHub

--- 3151-special-array-i/3151-special-array-i.py
+++ 3151-special-array-i/3151-special-array-i.py
@@ -1,15 +1,6 @@
 class Solution:
     def isArraySpecial(self, nums: List[int]) -> bool:
-        is_even = nums[0] & 1
         for i in range(1, len(nums)):
-            if is_even:
-                if nums[i] & 1 == 0:
-                    is_even = 0
-                else:
-                    return False
-            else:
-                if nums[i] & 1 != 0:
-                    is_even = 1
-                else:
-                    return False
+            if (nums[i - 1] & 1) ^ (nums[i] & 1) == 0:
+                return False
         return True
