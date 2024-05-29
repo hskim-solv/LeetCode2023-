c890980690050e794e14485adff1467dc605ed8d
hyeongseop kim <times21c@gmail.com>
2024-05-29 16:55:22 +0900

    Time: 39 ms (99.01%), Space: 16.6 MB (62.53%) - LeetHub

---
+++ 3162-find-the-number-of-good-pairs-i/3162-find-the-number-of-good-pairs-i.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numberOfPairs(self, nums1: List[int], nums2: List[int], k: int) -> int:
+        res = 0
+        for n1 in nums1:
+            for n2 in nums2:
+                n2k = n2*k
+                if n1 >= n2k and n1 % n2k == 0:
+                    res += 1
+        return res
